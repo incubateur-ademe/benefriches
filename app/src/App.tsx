5c397d081b01d4632a990affae2db1be61dd96e4
Claire Dhénin <claire.dhenin@nebulae.co>
2023-07-12 10:48:00 +0200

    Add Docker for development

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello Bénéfriches !</h1>
+      <h1>Hello Bénéfriches from Docker !</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
