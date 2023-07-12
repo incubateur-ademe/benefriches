fadaefb08a786cac9eacd8a4ff980098fb420073
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-07-12 15:27:16 +0200

    run prettier on webapp source code

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 3000, // you can replace this port with any port
-  }
-})
+  },
+});
