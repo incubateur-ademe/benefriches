64f7f7ffa339d3f2dbe477c1e7a6e7f92f76f546
Claire Dhénin <claire.dhenin@nebulae.co>
2023-07-12 11:05:49 +0200

    Fix docker-compose.yml

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    port: 5173, // you can replace this port with any port
+    port: 3000, // you can replace this port with any port
   }
 })
