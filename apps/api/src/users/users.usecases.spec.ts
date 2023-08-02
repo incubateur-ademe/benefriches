b4a79a5fe4aa53c3b5ac1fffbdee675e606dac4d
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-02 11:50:11 +0200

    Use zod to validate user email and password

--- apps/api/src/users/users.usecases.spec.ts
+++ apps/api/src/users/users.usecases.spec.ts
@@ -2,11 +2,33 @@ import { createUser } from "./users.usecases";
 
 describe("Register", () => {
   test("User account creation", () => {
-    expect(createUser("test@beta.gouv.fr", "mypassword")).toEqual({ "email": "test@beta.gouv.fr", "password": "mypassword"});
+    expect(createUser("test@beta.gouv.fr", "mypassword123456789")).toEqual({
+      email: "test@beta.gouv.fr",
+      password: "mypassword123456789",
+    });
   });
 
   test("User account creation : check password", () => {
     //@ts-expect-error
-    expect(() => createUser("test@beta.gouv.fr")).toThrowError("Password is required");
+    expect(() => createUser("test@beta.gouv.fr")).toThrowError(
+      "Password is required",
+    );
+  });
+
+  test("Cannot create account without email", () => {
+    //@ts-expect-error
+    expect(() => createUser(null)).toThrowError("Email is required");
+  });
+
+  test("Cannot create account with invalid email", () => {
+    expect(() => createUser("invalid-email", "mypasword")).toThrowError(
+      "Email is invalid",
+    );
+  });
+
+  test("Cannot create account with password too short", () => {
+    expect(() => createUser("test@beta.gouv.fr", "mypasword")).toThrowError(
+      "Password should be 12 or more characters",
+    );
   });
 });
--- apps/api/src/users/users.usecases.ts
+++ apps/api/src/users/users.usecases.ts
@@ -1,9 +1,20 @@
+import z from "zod";
 
 export function createUser(email: string, password: string) {
-  if (!password) { throw new Error("Password is required")}
+  if (!email) throw new Error("Email is required");
+
+  const validEmail = z
+    .string()
+    .email({ message: "Email is invalid" })
+    .parse(email);
+  if (!password) throw new Error("Password is required");
+  const validPassword = z
+    .string()
+    .min(12, "Password should be 12 or more characters")
+    .parse(password);
 
   return {
-    "email": email,
-    "password": password
+    email: email,
+    password: password,
   };
 }
