0adfabf3ecccbf9a326037f06fadf5b0002e5737
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-08 10:32:42 +0200

    Create Email and Password value objets to use in User entity

--- apps/api/src/users/domain/models/user.ts
+++ apps/api/src/users/domain/models/user.ts
@@ -1,5 +1,19 @@
-export type User = {
+import { EmailAddress } from "./emailAddress";
+
+type UserProps = {
   id: string;
-  email: string;
+  email: EmailAddress;
   password: string;
 };
+
+export class User {
+  private constructor(
+    readonly id: string,
+    readonly email: string,
+    readonly password: string,
+  ) {}
+
+  static create({ id, email, password }: UserProps): User {
+    return new User(id, email.getValue(), password);
+  }
+}
