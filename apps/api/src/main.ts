cd08df934da8e6361039fd2eb924126c65080586
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-11 15:36:10 +0200

    Make API listening port configurable through env

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+const port = Number(process.env.PORT) || 3000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  await app.listen(3000);
+  await app.listen(port);
 }
 void bootstrap();
