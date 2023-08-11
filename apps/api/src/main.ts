648347f27757567c0d4e3b9c683954e6fb14a8a0
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-11 15:50:15 +0200

    Fix .lintstaged in api and web

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-const port = Number(process.env.PORT) || 3000
+const port = Number(process.env.PORT) || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
