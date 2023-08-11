99defe63c7843b962a5ece683beeca509e464f3d
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-11 16:08:20 +0200

    Fix api build script for scalingo

--- apps/api/scripts/build-for-scalingo.sh
+++ apps/api/scripts/build-for-scalingo.sh
@@ -21,13 +21,14 @@ cp -v apps/api/scalingo/.buildpacks apps/api/scalingo/Procfile .tmp-build/
 
 # The buildpack used to deploy api (see Procfile) runs "pnpm install", "pnpm build" and "pnpm prune --prod"
 # Our code is already built and we don't include the source so we skip the "build" in root package.json
-sed -i "" 's/pnpm run -r build/echo 1/' .tmp-build/package.json
+# /!\ WARNING /!\ these sed commands are NOT compatible with MacOS (add "" after -i if you want to run it on your laptop)
+sed -i 's/pnpm run -r build/echo 1/' .tmp-build/package.json
 # We don't need these
-sed -i "" /\"prepare\":/d .tmp-build/package.json
-sed -i "" /\"lint\":/d .tmp-build/package.json
-sed -i "" /\"format\":/d .tmp-build/package.json
-sed -i "" /\"format:check\":/d .tmp-build/package.json
-sed -i "" /\"typecheck\":/d .tmp-build/package.json
+sed -i /\"prepare\":/d .tmp-build/package.json
+sed -i /\"lint\":/d .tmp-build/package.json
+sed -i /\"format\":/d .tmp-build/package.json
+sed -i /\"format:check\":/d .tmp-build/package.json
+sed -i /\"typecheck\":/d .tmp-build/package.json
 
 # Make archive to upload the packaged built api
 tar -czf api-scalingo.tar.gz .tmp-build
