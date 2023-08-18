7478196a5d00f0e0df09fc9f0ffc3c3b0d32e0f4
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-18 15:26:27 +0200

    Use ts-jest instead of babel-preset-typescript to read Typescript file in test
    ts-jest is closer to what tsc does and we don't need to add babel plugins (decorators for example)

--- apps/api/test/jest-integration.config.ts
+++ apps/api/test/jest-integration.config.ts
@@ -1,9 +1,11 @@
+import type { JestConfigWithTsJest } from "ts-jest";
 import baseConfig from "./jest.config";
 
-/** @type {import('jest').Config} */
-module.exports = {
+const config: JestConfigWithTsJest = {
   ...baseConfig,
   testMatch: ["<rootDir>/src/**/*.integration-spec.ts"],
   globalSetup: "<rootDir>/test/integration-tests-setup.ts",
   globalTeardown: "<rootDir>/test/integration-tests-teardown.ts",
 };
+
+export default config;
