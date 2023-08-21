c3f69e056ab24c84d73d24c2cd94018579508034
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-21 16:54:19 +0200

    Remove useless eslint-disable

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-// eslint-disable-next-line @typescript-eslint/no-magic-numbers
 const port = Number(process.env.PORT) || 3000;
 
 async function bootstrap() {
