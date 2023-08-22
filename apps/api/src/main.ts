8be131d5eaebce354279bfb1c7a0c810678a46b6
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-22 16:12:04 +0200

    Make API bootstrap fail fast when db connection cannot be established

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
+import { SqlConnection } from "./shared-kernel/adapters/sql-knex/sqlConnection.module";
+import { Knex } from "knex";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // test SQL connection so we fail fast if DB is not accesible
+  const sqlConnection: Knex = app.get(SqlConnection);
+  try {
+    await sqlConnection.raw("SELECT 1");
+  } catch (err) {
+    console.error("Error: could not establish SQL connection");
+    console.error(err);
+    await sqlConnection.destroy();
+    process.exit(1);
+  }
+
+  // run http server
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow("PORT"));
 }
