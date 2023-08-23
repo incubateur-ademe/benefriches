0f494575d01f0e173bd5d42eafb6c8cfaffb10c3
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-23 13:53:32 +0200

    Better db config

--- apps/api/src/shared-kernel/adapters/sql-knex/knexConfig.ts
+++ apps/api/src/shared-kernel/adapters/sql-knex/knexConfig.ts
@@ -3,20 +3,27 @@ import type { Knex } from "knex";
 const config: Knex.Config = {
   client: "pg",
   connection: () => {
+    const connection: Knex.Config["connection"] = {};
+
+    if (process.env.NODE_ENV === "production") {
+      connection.ssl = {
+        rejectUnauthorized: false,
+      };
+    }
+
     return process.env.DATABASE_URL
       ? {
+          ...connection,
           connectionString: process.env.DATABASE_URL,
         }
       : {
+          ...connection,
           port: process.env.DATABASE_PORT
             ? Number(process.env.DATABASE_PORT)
             : 5432,
           user: process.env.DATABASE_USER,
           password: process.env.DATABASE_PASSWORD,
           database: process.env.DATABASE_DB_NAME,
-          ssl: {
-            rejectUnauthorized: process.env.NODE_ENV !== "production",
-          },
         };
   },
   migrations: {
