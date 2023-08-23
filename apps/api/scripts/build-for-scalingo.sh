308afe65a79bdd69998afab93cc4031cc9c49184
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-23 14:43:32 +0200

    Fix build to allow migration script to run on scalingo

--- apps/api/scripts/build-for-scalingo.sh
+++ apps/api/scripts/build-for-scalingo.sh
@@ -21,7 +21,7 @@ cp -v apps/api/scalingo/.buildpacks apps/api/scalingo/Procfile .tmp-build/
 
 # The buildpack used to deploy api (see Procfile) runs "pnpm install", "pnpm build" and "pnpm prune --prod"
 # Our code is already built and we don't include the source so we skip the "build" in root package.json
-# /!\ WARNING /!\ these sed commands are NOT compatible with MacOS (add "" after -i if you want to run it on your laptop)
+# /!\ WARNING /!\ these sed commands are NOT compatible with MacOS (add '' after -i if you want to run it on your laptop)
 sed -i 's/pnpm run -r build/echo 1/' .tmp-build/package.json
 # We don't need these
 sed -i /\"prepare\":/d .tmp-build/package.json
@@ -29,6 +29,9 @@ sed -i /\"lint\":/d .tmp-build/package.json
 sed -i /\"format\":/d .tmp-build/package.json
 sed -i /\"format:check\":/d .tmp-build/package.json
 sed -i /\"typecheck\":/d .tmp-build/package.json
+
+# Replace knexfile path with build (now located in dist/ folder) and use .js extension since it's transpiled
+sed -i 's/src\/shared-kernel\/adapters\/sql-knex\/knexfile.ts/dist\/src\/shared-kernel\/adapters\/sql-knex\/knexfile.js/' .tmp-build/apps/api/package.json
 
 # Make archive to upload the packaged built api
 tar -czf api-scalingo.tar.gz .tmp-build
