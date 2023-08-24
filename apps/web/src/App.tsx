c0d3414724157f19cefd71d9194bf44f7623f83e
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-08-24 15:57:35 +0200

    Use exported names from router instead of magic strings in webapp

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,6 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import HeaderFooterLayout from "./components/layout/HeaderFooterLayout/HeaderFooterLayout";
-import { useRoute } from "./router";
+import { useRoute, routes } from "./router";
 import HomePage from "./components/pages/Home";
 import SiteFoncierCreationForm from "./components/pages/SiteFoncier/Creation/Index";
 import SitesFonciersListPage from "./components/pages/SiteFoncier/List";
@@ -14,17 +14,18 @@ function App() {
   return (
     <HeaderFooterLayout>
       <main className={fr.cx("fr-container", "fr-py-10w")}>
-        {/* TODO: exporter les noms de route depuis le router ou ailleurs pour éviter d'avoir des magic string */}
-        {route.name === "home" && <HomePage />}
-        {route.name === "mesSitesFonciers" && <SitesFonciersListPage />}
-        {route.name === "siteFoncierDetails" && (
+        {route.name === routes.home.name && <HomePage />}
+        {route.name === routes.mesSitesFonciers.name && (
+          <SitesFonciersListPage />
+        )}
+        {route.name === routes.siteFoncierDetails.name && (
           <SiteFoncierDetailsPage
             siteFoncierName={route.params.siteFoncierName}
           />
         )}
-        {route.name === "login" && <LoginPage />}
-        {route.name === "createUser" && <CreateUserPage />}
-        {route.name === "siteFoncierForm" && (
+        {route.name === routes.login.name && <LoginPage />}
+        {route.name === routes.createUser.name && <CreateUserPage />}
+        {route.name === routes.siteFoncierForm.name && (
           <SiteFoncierCreationForm route={route} />
         )}
         {route.name === false && <>Page non trouvée</>}
