f342d57de402716d438d9ccb8ea0ef4106602e45
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-09-27 14:47:36 +0200

    Rework natural area creation navigation code + add missing unit tests

--- apps/web/src/store/features/naturalAreaCreation.spec.ts
+++ apps/web/src/store/features/naturalAreaCreation.spec.ts
@@ -1,5 +1,6 @@
 import reducer, {
   NaturalAreaCreationStep,
+  setForestTrees,
   setSpacesSurfaceArea,
   setSpacesTypes,
 } from "./naturalAreaCreation";
@@ -7,55 +8,24 @@ import reducer, {
 import {
   NaturalAreaSpaceType,
   SiteFoncierType,
+  TreeType,
 } from "@/components/pages/SiteFoncier/siteFoncier";
 
 describe("Natural Area creation flow", () => {
-  it("when spaces have been set, new step should be SPACES_SURFACES and following steps should be set accordingly", () => {
-    const spaces = [
-      NaturalAreaSpaceType.FOREST,
-      NaturalAreaSpaceType.ORCHARD,
-      NaturalAreaSpaceType.PRAIRIE,
-    ];
-    const action = setSpacesTypes(spaces);
-    const newState = reducer(undefined, action);
-    expect(newState).toEqual({
-      naturalAreaData: {
-        type: SiteFoncierType.NATURAL_AREA,
-        spaces: [
-          { type: NaturalAreaSpaceType.FOREST },
-          { type: NaturalAreaSpaceType.ORCHARD },
-          { type: NaturalAreaSpaceType.PRAIRIE },
-        ],
-      },
-      step: NaturalAreaCreationStep.SPACES_SURFACE_AREA_STEP,
-      nextSteps: [
-        NaturalAreaCreationStep.FOREST_TREES_STEP,
-        NaturalAreaCreationStep.PRAIRIE_VEGETATION_STEP,
-        NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
-        NaturalAreaCreationStep.CARBON_SUMMARY_STEP,
-        NaturalAreaCreationStep.OWNER_STEP,
-        NaturalAreaCreationStep.NAMING_STEP,
-        NaturalAreaCreationStep.CONFIRMATION_STEP,
-      ],
-    });
-  });
-
-  describe("setSpacesSurfaceArea", () => {
-    it("when the natural area is a forest, next step should be FOREST_TREES", () => {
+  describe("setSpacesTypes", () => {
+    it("when natureal area is a forest, next step should be SPACES_SURFACES then FOREST_TREES", () => {
       const spaces = [NaturalAreaSpaceType.FOREST];
-      const initialState = reducer(undefined, setSpacesTypes(spaces));
+      const action = setSpacesTypes(spaces);
+      const newState = reducer(undefined, action);
 
-      const newState = reducer(
-        initialState,
-        setSpacesSurfaceArea({ [NaturalAreaSpaceType.FOREST]: 10000 }),
-      );
       expect(newState).toEqual({
         naturalAreaData: {
           type: SiteFoncierType.NATURAL_AREA,
-          spaces: [{ type: NaturalAreaSpaceType.FOREST, surface: 10000 }],
+          spaces: [{ type: NaturalAreaSpaceType.FOREST }],
         },
-        step: NaturalAreaCreationStep.FOREST_TREES_STEP,
+        step: NaturalAreaCreationStep.SPACES_SURFACE_AREA_STEP,
         nextSteps: [
+          NaturalAreaCreationStep.FOREST_TREES_STEP,
           NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
           NaturalAreaCreationStep.CARBON_SUMMARY_STEP,
           NaturalAreaCreationStep.OWNER_STEP,
@@ -65,20 +35,197 @@ describe("Natural Area creation flow", () => {
       });
     });
 
-    it("when the natural area is a mix of forest and prairie, next step should be FOREST_TREES then soil summary", () => {
-      const spaces = [NaturalAreaSpaceType.FOREST];
-      const initialState = reducer(undefined, setSpacesTypes(spaces));
+    it("when natureal area is a mix of prairie, forest and water, next step should be SPACES_SURFACES then FOREST_TREES and PRAIRIE_VEGETATION", () => {
+      const spaces = [
+        NaturalAreaSpaceType.FOREST,
+        NaturalAreaSpaceType.PRAIRIE,
+        NaturalAreaSpaceType.WATER,
+      ];
+      const action = setSpacesTypes(spaces);
+      const newState = reducer(undefined, action);
 
-      const state = reducer(
-        initialState,
-        setSpacesSurfaceArea({ [NaturalAreaSpaceType.FOREST]: 10000 }),
-      );
-      expect(state).toEqual({
+      expect(newState).toEqual({
         naturalAreaData: {
           type: SiteFoncierType.NATURAL_AREA,
-          spaces: [{ type: NaturalAreaSpaceType.FOREST, surface: 10000 }],
+          spaces: [
+            { type: NaturalAreaSpaceType.FOREST },
+            { type: NaturalAreaSpaceType.PRAIRIE },
+            { type: NaturalAreaSpaceType.WATER },
+          ],
         },
-        step: NaturalAreaCreationStep.FOREST_TREES_STEP,
+        step: NaturalAreaCreationStep.SPACES_SURFACE_AREA_STEP,
+        nextSteps: [
+          NaturalAreaCreationStep.FOREST_TREES_STEP,
+          NaturalAreaCreationStep.PRAIRIE_VEGETATION_STEP,
+          NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
+          NaturalAreaCreationStep.CARBON_SUMMARY_STEP,
+          NaturalAreaCreationStep.OWNER_STEP,
+          NaturalAreaCreationStep.NAMING_STEP,
+          NaturalAreaCreationStep.CONFIRMATION_STEP,
+        ],
+      });
+    });
+
+    it("when natureal area is a mix of orchard, cultivation, vineyard, wet land and water, next step should be SPACES_SURFACES then SOIL_SUMMARY", () => {
+      const spaces = [
+        NaturalAreaSpaceType.CULTIVATION,
+        NaturalAreaSpaceType.ORCHARD,
+        NaturalAreaSpaceType.WET_LAND,
+        NaturalAreaSpaceType.WATER,
+        NaturalAreaSpaceType.VINEYARD,
+      ];
+      const action = setSpacesTypes(spaces);
+      const newState = reducer(undefined, action);
+
+      expect(newState).toEqual({
+        naturalAreaData: {
+          type: SiteFoncierType.NATURAL_AREA,
+          spaces: [
+            { type: NaturalAreaSpaceType.CULTIVATION },
+            { type: NaturalAreaSpaceType.ORCHARD },
+            { type: NaturalAreaSpaceType.WET_LAND },
+            { type: NaturalAreaSpaceType.WATER },
+            { type: NaturalAreaSpaceType.VINEYARD },
+          ],
+        },
+        step: NaturalAreaCreationStep.SPACES_SURFACE_AREA_STEP,
+        nextSteps: [
+          NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
+          NaturalAreaCreationStep.CARBON_SUMMARY_STEP,
+          NaturalAreaCreationStep.OWNER_STEP,
+          NaturalAreaCreationStep.NAMING_STEP,
+          NaturalAreaCreationStep.CONFIRMATION_STEP,
+        ],
+      });
+    });
+  });
+
+  describe("setSpacesSurfaceArea", () => {
+    it("should set surface area for given spaces", () => {
+      const action = setSpacesSurfaceArea({
+        [NaturalAreaSpaceType.FOREST]: 5000,
+        [NaturalAreaSpaceType.PRAIRIE]: 10000,
+        [NaturalAreaSpaceType.WATER]: 500,
+      });
+      const newState = reducer(
+        {
+          naturalAreaData: { type: SiteFoncierType.NATURAL_AREA },
+          step: NaturalAreaCreationStep.SPACES_SURFACE_AREA_STEP,
+          nextSteps: [
+            NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
+            NaturalAreaCreationStep.CARBON_SUMMARY_STEP,
+            NaturalAreaCreationStep.OWNER_STEP,
+            NaturalAreaCreationStep.NAMING_STEP,
+            NaturalAreaCreationStep.CONFIRMATION_STEP,
+          ],
+        },
+        action,
+      );
+
+      expect(newState).toEqual({
+        naturalAreaData: {
+          type: SiteFoncierType.NATURAL_AREA,
+          spaces: [
+            { type: NaturalAreaSpaceType.FOREST, surface: 5000 },
+            { type: NaturalAreaSpaceType.PRAIRIE, surface: 10000 },
+            { type: NaturalAreaSpaceType.WATER, surface: 500 },
+          ],
+        },
+        step: NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
+        nextSteps: [
+          NaturalAreaCreationStep.CARBON_SUMMARY_STEP,
+          NaturalAreaCreationStep.OWNER_STEP,
+          NaturalAreaCreationStep.NAMING_STEP,
+          NaturalAreaCreationStep.CONFIRMATION_STEP,
+        ],
+      });
+    });
+  });
+
+  describe("setForestTrees", () => {
+    it("when only one type of trees on forest, next step should be the expected one", () => {
+      const action = setForestTrees([TreeType.RESINOUS]);
+      const newState = reducer(
+        {
+          naturalAreaData: {
+            type: SiteFoncierType.NATURAL_AREA,
+            spaces: [
+              { type: NaturalAreaSpaceType.FOREST, surface: 5000 },
+              { type: NaturalAreaSpaceType.PRAIRIE, surface: 10000 },
+            ],
+          },
+          step: NaturalAreaCreationStep.FOREST_TREES_STEP,
+          nextSteps: [
+            NaturalAreaCreationStep.PRAIRIE_VEGETATION_STEP,
+            NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
+            NaturalAreaCreationStep.CARBON_SUMMARY_STEP,
+            NaturalAreaCreationStep.OWNER_STEP,
+            NaturalAreaCreationStep.NAMING_STEP,
+            NaturalAreaCreationStep.CONFIRMATION_STEP,
+          ],
+        },
+        action,
+      );
+
+      expect(newState).toEqual({
+        naturalAreaData: {
+          type: SiteFoncierType.NATURAL_AREA,
+          spaces: [
+            {
+              type: NaturalAreaSpaceType.FOREST,
+              surface: 5000,
+              trees: [{ type: TreeType.RESINOUS }],
+            },
+            { type: NaturalAreaSpaceType.PRAIRIE, surface: 10000 },
+          ],
+        },
+        step: NaturalAreaCreationStep.PRAIRIE_VEGETATION_STEP,
+        nextSteps: [
+          NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
+          NaturalAreaCreationStep.CARBON_SUMMARY_STEP,
+          NaturalAreaCreationStep.OWNER_STEP,
+          NaturalAreaCreationStep.NAMING_STEP,
+          NaturalAreaCreationStep.CONFIRMATION_STEP,
+        ],
+      });
+    });
+    it("when multiple type of trees on forest, next step should FOREST_TREES_DISTRIBUTION", () => {
+      const action = setForestTrees([TreeType.RESINOUS, TreeType.POPLAR]);
+      const newState = reducer(
+        {
+          naturalAreaData: {
+            type: SiteFoncierType.NATURAL_AREA,
+            spaces: [
+              { type: NaturalAreaSpaceType.FOREST, surface: 5000 },
+              { type: NaturalAreaSpaceType.PRAIRIE, surface: 10000 },
+            ],
+          },
+          step: NaturalAreaCreationStep.FOREST_TREES_STEP,
+          nextSteps: [
+            NaturalAreaCreationStep.PRAIRIE_VEGETATION_STEP,
+            NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
+            NaturalAreaCreationStep.CARBON_SUMMARY_STEP,
+            NaturalAreaCreationStep.OWNER_STEP,
+            NaturalAreaCreationStep.NAMING_STEP,
+            NaturalAreaCreationStep.CONFIRMATION_STEP,
+          ],
+        },
+        action,
+      );
+
+      expect(newState).toEqual({
+        naturalAreaData: {
+          type: SiteFoncierType.NATURAL_AREA,
+          spaces: [
+            {
+              type: NaturalAreaSpaceType.FOREST,
+              surface: 5000,
+              trees: [{ type: TreeType.RESINOUS }, { type: TreeType.POPLAR }],
+            },
+            { type: NaturalAreaSpaceType.PRAIRIE, surface: 10000 },
+          ],
+        },
+        step: NaturalAreaCreationStep.FOREST_TREES_DISTRIBUTION,
         nextSteps: [
           NaturalAreaCreationStep.PRAIRIE_VEGETATION_STEP,
           NaturalAreaCreationStep.SOIL_SUMMARY_STEP,
