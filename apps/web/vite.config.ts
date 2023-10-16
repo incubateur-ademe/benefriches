b1a41274beb14999c89ed8335375289642332ced
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-10-16 15:34:16 +0200

    Proxy http requests from /api/* in webapp

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,18 +1,31 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const serverProxyConfig = {
+    "/api": {
+      target: env.API_HOST,
+      changeOrigin: true,
+      rewrite: (path: string) => path.replace(/^\/api/, ""),
     },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 3000, // you can replace this port with any port
-  },
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
+  };
+
+  return {
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      strictPort: true,
+      port: 3000, // you can replace this port with any port
+      proxy: serverProxyConfig,
+    },
+    resolve: {
+      alias: [{ find: "@", replacement: "/src" }],
+    },
+  };
 });
