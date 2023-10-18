1e707f6424a1c7cb40d2985115118c90f69aa7d4
Claire Dhénin <25612600+cdhenin@users.noreply.github.com>
2023-10-18 15:30:15 +0200

    add computation usecase

---
+++ apps/api/src/carbon-storage/domain/usecases/getCityCarbonStoragePerSoilsCategory.ts
@@ -0,0 +1,54 @@
+import { UseCase } from "../../../shared-kernel/usecase";
+import { CarbonStorageRepository } from "../gateways/CarbonStorageRepository";
+import { SoilCategoryType } from "../models/carbonStorage";
+
+type Request = {
+  cityCode: string;
+  soils: { surfaceArea: number; type: SoilCategoryType }[];
+};
+
+type Response = {
+  totalCarbonStorage: number;
+  soilsCarbonStorage: {
+    surfaceArea: number;
+    carbonStorage: number;
+    type: SoilCategoryType;
+  }[];
+};
+
+export class GetCityCarbonStoragePerSoilsCategoryUseCase
+  implements UseCase<Request, Response>
+{
+  constructor(
+    private readonly carbonStorageRepository: CarbonStorageRepository,
+  ) {}
+
+  async execute({ cityCode, soils = [] }: Request): Promise<Response> {
+    const carbonStorage =
+      await this.carbonStorageRepository.getCarbonStorageForCity(
+        cityCode,
+        soils.map(({ type }) => type),
+      );
+
+    const soilsCarbonStorage = soils.map(({ type, surfaceArea }) => {
+      const entriesForCategory = carbonStorage.filter(
+        ({ soil_category }) => soil_category === type,
+      );
+      const totalForCategory = entriesForCategory.reduce(
+        (total, { stock_tC_by_ha }) => total + stock_tC_by_ha * surfaceArea,
+        0,
+      );
+      return { type, surfaceArea, carbonStorage: totalForCategory };
+    });
+
+    const totalCarbonStorage = Object.values(soilsCarbonStorage).reduce(
+      (total, { carbonStorage }) => total + carbonStorage,
+      0,
+    );
+
+    return {
+      totalCarbonStorage: Math.round(totalCarbonStorage * 100) / 100,
+      soilsCarbonStorage,
+    };
+  }
+}
