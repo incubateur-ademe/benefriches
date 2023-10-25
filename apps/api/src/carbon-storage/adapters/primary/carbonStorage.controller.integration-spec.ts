6a43619f9ce41560c58e1791983a44fcdc7eb064
Claire Dhénin <25612600+cdhenin@users.noreply.github.com>
2023-10-25 10:42:12 +0200

    adapt the endpoint signature to front

--- apps/api/src/carbon-storage/adapters/primary/carbonStorage.controller.integration-spec.ts
+++ apps/api/src/carbon-storage/adapters/primary/carbonStorage.controller.integration-spec.ts
@@ -38,6 +38,14 @@ describe("CarbonStorage controller", () => {
       expect(response.status).toEqual(400);
     });
 
+    it("can't return information if there is no soils indication", async () => {
+      const response = await supertest(app.getHttpServer()).get(
+        "/site-soils-carbon-sequestration?cityCode=01081",
+      );
+
+      expect(response.status).toEqual(400);
+    });
+
     it("returns an object with totalCarbonStorage and soilsCarbonStorage", async () => {
       const response = await supertest(app.getHttpServer()).get(
         "/site-soils-carbon-sequestration?cityCode=01081&soils[0][surfaceArea]=1.5&soils[0][type]=CULTIVATION&soils[1][surfaceArea]=3&soils[1][type]=FOREST_DECIDUOUS",
@@ -46,18 +54,10 @@ describe("CarbonStorage controller", () => {
       expect(response.status).toEqual(200);
       expect(response.body).toEqual({
         totalCarbonStorage: 739.11,
-        soilsCarbonStorage: [
-          {
-            type: SoilCategoryType.CULTIVATION.toUpperCase(),
-            surfaceArea: 1.5,
-            carbonStorage: 82.5,
-          },
-          {
-            type: SoilCategoryType.FOREST_DECIDUOUS.toUpperCase(),
-            surfaceArea: 3,
-            carbonStorage: 656.61,
-          },
-        ],
+        soilsStorage: {
+          [SoilCategoryType.CULTIVATION.toUpperCase()]: 82.5,
+          [SoilCategoryType.FOREST_DECIDUOUS.toUpperCase()]: 656.61,
+        },
       });
     });
   });
