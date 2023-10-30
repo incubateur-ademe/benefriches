bc1550d29be8665d1f8bb0fd96348639cb7e923b
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-10-30 14:06:27 +0100

    Create objectToQueryParameter util

---
+++ apps/web/src/shared/services/object-query-parameters/objectToQueryParameters.ts
@@ -0,0 +1,33 @@
+type StringableValue = string | number | boolean;
+
+type QueryParamsObject = {
+  [key: string]:
+    | StringableValue
+    | StringableValue[]
+    | { [key: string]: StringableValue }[];
+};
+
+export const objectToQueryParams = (obj: QueryParamsObject) => {
+  const params = new URLSearchParams();
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (Array.isArray(value)) {
+      value.forEach((item, index) => {
+        if (typeof item === "object") {
+          for (const [nestedKey, nestedValue] of Object.entries(item)) {
+            params.append(
+              `${key}[${index}][${nestedKey}]`,
+              String(nestedValue),
+            );
+          }
+        } else {
+          params.append(`${key}[${index}]`, String(item));
+        }
+      });
+    } else {
+      params.append(key, String(value));
+    }
+  }
+
+  return params.toString();
+};
