7228c6d6dc1dff756d8b057d8f3374d47d9d5e87
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-11-02 12:29:31 +0100

    Create page for project stakeholders introduction

---
+++ apps/web/src/features/create-project/views/stakeholders/introduction/index.tsx
@@ -0,0 +1,21 @@
+import {
+  goToStep,
+  ProjectCreationStep,
+} from "../../../application/createProject.reducer";
+import ProjectStakeholdersIntroduction from "./StakeholdersIntroduction";
+
+import { useAppDispatch } from "@/shared/views/hooks/store.hooks";
+
+function ProjectStakeholdersIntroductionContainer() {
+  const dispatch = useAppDispatch();
+
+  return (
+    <ProjectStakeholdersIntroduction
+      onNext={() =>
+        dispatch(goToStep(ProjectCreationStep.STAKEHOLDERS_OPERATOR))
+      }
+    />
+  );
+}
+
+export default ProjectStakeholdersIntroductionContainer;
