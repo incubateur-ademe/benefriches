130c10229876ede1b86f2bd77d155c7b81f71449
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-11-29 11:50:44 +0100

    Extract generateSiteDesignation function from reducer

--- apps/web/src/features/create-site/application/createSite.reducer.ts
+++ apps/web/src/features/create-site/application/createSite.reducer.ts
@@ -15,51 +15,6 @@ export type SiteCreationState = {
   saveLoadingState: "idle" | "loading" | "success" | "error";
 };
 
-const isSoilTypePrairie = (soilType: SoilType) => {
-  return soilType.startsWith("PRAIRIE");
-};
-const isSoilTypeForest = (soilType: SoilType) => {
-  return soilType.startsWith("FOREST");
-};
-const isSoilTypeAgricultural = (soilType: SoilType) => {
-  return [SoilType.VINEYARD, SoilType.ORCHARD, SoilType.CULTIVATION].includes(
-    soilType,
-  );
-};
-const isSoilTypeArtificial = (soilType: SoilType) => {
-  return [
-    SoilType.ARTIFICIAL_GRASS_OR_BUSHES_FILLED,
-    SoilType.ARTIFICIAL_TREE_FILLED,
-    SoilType.MINERAL_SOIL,
-    SoilType.BUILDINGS,
-  ].includes(soilType);
-};
-const isSoilTypeNatural = (soilType: SoilType) => {
-  return (
-    isSoilTypePrairie(soilType) ||
-    isSoilTypeForest(soilType) ||
-    soilType === SoilType.WATER ||
-    soilType === SoilType.WET_LAND
-  );
-};
-
-export const getSiteTypeLabel = (siteData: SiteCreationState["siteData"]) => {
-  if (siteData.isFriche) return "friche";
-
-  const { soils = [] } = siteData;
-
-  const nonArtificialSoils = soils.filter(
-    (soilType) => !isSoilTypeArtificial(soilType),
-  );
-  if (nonArtificialSoils.length === 0) return "espace";
-  if (nonArtificialSoils.every(isSoilTypePrairie)) return "prairie";
-  if (nonArtificialSoils.every(isSoilTypeForest)) return "forêt";
-  if (nonArtificialSoils.every(isSoilTypeAgricultural))
-    return "espace agricole";
-  if (nonArtificialSoils.every(isSoilTypeNatural)) return "espace naturel";
-  return "espace naturel et agricole";
-};
-
 export enum SiteCreationStep {
   SITE_TYPE = "SITE_TYPE",
   ADDRESS = "ADDRESS",
