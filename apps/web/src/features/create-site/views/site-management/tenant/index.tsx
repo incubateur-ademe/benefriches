801bf2800a0edda17416c49a8ea6735453a0ec0a
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2023-12-05 10:53:11 +0100

    Skip full-time jobs forms for now

--- apps/web/src/features/create-site/views/site-management/tenant/index.tsx
+++ apps/web/src/features/create-site/views/site-management/tenant/index.tsx
@@ -5,10 +5,14 @@ import {
   setTenant,
   SiteCreationStep,
 } from "@/features/create-site/application/createSite.reducer";
-import { useAppDispatch } from "@/shared/views/hooks/store.hooks";
+import { SiteDraft } from "@/features/create-site/domain/siteFoncier.types";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "@/shared/views/hooks/store.hooks";
 import { AppDispatch } from "@/store";
 
-const mapProps = (dispatch: AppDispatch) => {
+const mapProps = (dispatch: AppDispatch, siteData: Partial<SiteDraft>) => {
   return {
     onSubmit: (data: FormValues) => {
       if (data.tenantType === "company") {
@@ -27,15 +31,19 @@ const mapProps = (dispatch: AppDispatch) => {
           }),
         );
       }
-      dispatch(goToStep(SiteCreationStep.FULL_TIME_JOBS_INVOLVED));
+      const nextStep = siteData.isFriche
+        ? SiteCreationStep.RECENT_ACCIDENTS
+        : SiteCreationStep.YEARLY_EXPENSES;
+      dispatch(goToStep(nextStep));
     },
   };
 };
 
 function FricheTenantFormContainer() {
   const dispatch = useAppDispatch();
+  const siteDraft = useAppSelector((state) => state.siteCreation.siteData);
 
-  return <FricheTenantForm {...mapProps(dispatch)} />;
+  return <FricheTenantForm {...mapProps(dispatch, siteDraft)} />;
 }
 
 export default FricheTenantFormContainer;
