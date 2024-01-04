e6f6a193cbece6ba020f0d96e32a75140d4300fa
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-01-04 18:01:03 +0100

    Use same prettier config for api and web

---
+++ .tooling/prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParens: "always",
+    bracketSameLine: false,
+    bracketSpacing: true,
+    jsxSingleQuote: false,
+    printWidth: 100,
+    quoteProps: "as-needed",
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    trailingComma: "all",
+    useTabs: false,
+  };
+  
--- apps/api/.prettierrc.cjs
+++ apps/api/.prettierrc.cjs
@@ -1,13 +1,1 @@
-module.exports = {
-  arrowParens: "always",
-  bracketSameLine: false,
-  bracketSpacing: true,
-  jsxSingleQuote: false,
-  printWidth: 100,
-  quoteProps: "as-needed",
-  semi: true,
-  singleQuote: false,
-  tabWidth: 2,
-  trailingComma: "all",
-  useTabs: false,
-};
+module.exports = require("../../.tooling/prettierrc");
