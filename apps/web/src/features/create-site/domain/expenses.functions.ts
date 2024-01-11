999d003285ea3b9b88fe487a65a8ffdc3b4f481f
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-01-11 18:15:42 +0100

    fix(web): Fix conflicting eslint prefer-reduce-type-parameter with typescript check

--- apps/web/src/features/create-site/domain/expenses.functions.ts
+++ apps/web/src/features/create-site/domain/expenses.functions.ts
@@ -5,11 +5,16 @@ type GroupedByExpensesKey = "bearer" | "category";
 const groupAndSumBy =
   <K extends GroupedByExpensesKey>(key: K) =>
   (expenses: Expense[]) => {
-    return expenses.reduce<Record<Expense[K], number>>((expensesByKey, expense) => {
-      const group = expense[key];
-      const totalAmountForKey = expense.amount + expensesByKey[group];
-      return { ...expensesByKey, [group]: totalAmountForKey };
-    }, {});
+    return expenses.reduce<Record<Expense[K], number>>(
+      (expensesByKey, expense) => {
+        const group = expense[key];
+        const totalAmountForKey = expense.amount + expensesByKey[group];
+        expensesByKey[group] = totalAmountForKey;
+        return expensesByKey;
+      },
+      // eslint-disable-next-line @typescript-eslint/prefer-reduce-type-parameter
+      {} as Record<Expense[K], number>,
+    );
   };
 
 export const groupExpensesByBearer = (expenses: Expense[]) => {
