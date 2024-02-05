d2a1076985677b1c84016cd18f45a2843d47270f
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-02-05 18:46:20 +0100

    feat(api): extract site mock builders to own file

--- apps/api/src/sites/domain/usecases/createNewSite.usecase.spec.ts
+++ apps/api/src/sites/domain/usecases/createNewSite.usecase.spec.ts
@@ -2,60 +2,13 @@
 import { z } from "zod";
 import { DeterministicDateProvider } from "src/shared-kernel/adapters/date/DeterministicDateProvider";
 import { InMemorySiteRepository } from "src/sites/adapters/secondary/site-repository/InMemorySiteRepository";
-import {
-  CreateNewSiteUseCase,
-  DateProvider,
-  FricheSiteProps,
-  NonFricheSiteProps,
-} from "./createNewSite.usecase";
+import { buildFricheProps, buildMinimalSiteProps } from "../models/site.mock";
+import { CreateNewSiteUseCase, DateProvider } from "./createNewSite.usecase";
 
 describe("CreateNewSite Use Case", () => {
   let siteRepository: InMemorySiteRepository;
   let dateProvider: DateProvider;
   const fakeNow = new Date("2024-01-03T13:50:45");
-
-  const buildMinimalSiteProps = (
-    propsOverride?: Partial<NonFricheSiteProps>,
-  ): NonFricheSiteProps => {
-    return {
-      id: "28b53918-a6f6-43f2-9554-7b5434428f8b",
-      name: "My site",
-      isFriche: false,
-      surfaceArea: 15000,
-      owner: {
-        structureType: "department",
-        name: "Le département Paris",
-      },
-      soilsDistribution: {
-        BUILDINGS: 3000,
-        ARTIFICIAL_TREE_FILLED: 5000,
-        FOREST_MIXED: 60000,
-        MINERAL_SOIL: 5000,
-        IMPERMEABLE_SOILS: 1300,
-      },
-      yearlyExpenses: [],
-      yearlyIncomes: [],
-      address: {
-        city: "Paris",
-        cityCode: "75109",
-        postCode: "75009",
-        banId: "123abc",
-        lat: 48.876517,
-        long: 2.330785,
-        value: "1 rue de Londres, 75009 Paris",
-        streetName: "rue de Londres",
-      },
-      ...propsOverride,
-    };
-  };
-
-  const buildFricheProps = (propsOverride?: Partial<FricheSiteProps>): FricheSiteProps => {
-    return {
-      ...buildMinimalSiteProps(),
-      isFriche: true,
-      ...propsOverride,
-    };
-  };
 
   const getZodIssues = (err: unknown) => {
     if (err instanceof z.ZodError) {
