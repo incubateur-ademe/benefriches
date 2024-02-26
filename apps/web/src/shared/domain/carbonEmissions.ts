e0e184ed534f7dcee8fdd0e3cfbc4dfcd8d9653e
Claire Dhénin <25612600+cdhenin@users.noreply.github.com>
2024-02-26 17:42:22 +0100

    fix: computation of french carbon emission equivalent per year

--- apps/web/src/shared/domain/carbonEmissions.ts
+++ apps/web/src/shared/domain/carbonEmissions.ts
@@ -1,9 +1,18 @@
-const AVERAGE_FRENCH_ANNUAL_EMISSIONS_PER_PERSON = 9.9;
+const AVERAGE_FRENCH_ANNUAL_EMISSIONS_TON_CO2_EQ_PER_PERSON = 9.2;
+
+/* 
+Poids atomique d'un atome de carbone : 12
+Poids atomique d'un atome d’oxygène : 16
+Poids atomique total du CO2 = 44 (12 + (16 * 2)). 
+Quantité de carbone = Quantité de CO2 * 12 / 44 
+1 kg de CO2 = 0,27 kg de carbone
+*/
+const RATIO_CO2_TO_CARBON = 12 / 44;
 
 export const getCarbonTonsInAverageFrenchAnnualEmissionsPerPerson = (
   carbonTons: number,
 ): number => {
   if (!carbonTons) return 0;
 
-  return carbonTons / AVERAGE_FRENCH_ANNUAL_EMISSIONS_PER_PERSON;
+  return carbonTons / (AVERAGE_FRENCH_ANNUAL_EMISSIONS_TON_CO2_EQ_PER_PERSON * RATIO_CO2_TO_CARBON);
 };
