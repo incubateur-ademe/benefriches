41e3ecfb827eece930ad23396158badca985cd6b
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-03-28 14:56:05 +0100

    feat(web): wrap site creation introduction page in padded container

--- apps/web/src/features/create-site/views/introduction/CreateSiteIntroductionPage.tsx
+++ apps/web/src/features/create-site/views/introduction/CreateSiteIntroductionPage.tsx
@@ -16,48 +16,50 @@ function CreateSiteIntroductionPage() {
   }, [dispatch]);
 
   return (
-    <WizardFormLayout title="Tout commence sur un site">
-      <p>
-        Nous allons d’abord parler du <strong>site existant</strong> : la nature du site, la
-        typologie de ses sols, les dépenses associées, etc.
-      </p>
-      <p>
-        Une fois que ce site sera décrit, vous pourrez alors créer un ou plusieurs{" "}
-        <strong>projets d'aménagement</strong> sur ce site.
-      </p>
-      <Accordion label="Documents utiles" className={fr.cx("fr-mb-4w")}>
+    <section className={fr.cx("fr-container", "fr-py-4w")}>
+      <WizardFormLayout title="Tout commence sur un site">
         <p>
-          Pour vous aider à saisir les informations liées à votre projet, il peut être utile d'avoir
-          les ressources suivantes :
+          Nous allons d’abord parler du <strong>site existant</strong> : la nature du site, la
+          typologie de ses sols, les dépenses associées, etc.
         </p>
-        <ul>
-          <li>Plan cadastral du site</li>
-          <li>Acte de vente</li>
-          <li>
-            Logiciel de cartographie satellite (exemple:{" "}
-            <a href="https://www.openstreetmap.fr/" rel="noopener noreferrer" target="_blank">
-              OpenStreetMap
-            </a>
-            )
-          </li>
-          <li>Photographies</li>
-          <li>
-            Carte des anciens sites industriels et de services (
-            <a
-              href="https://infoterre.brgm.fr/viewer/MainTileForward.do"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              https://infoterre.brgm.fr/viewer/MainTileForward.do
-            </a>
-            )
-          </li>
-          <li>Rapports de diagnostics de pollution ou plan de gestion</li>
-          <li>Compte de résultat ou données économiques du site (gardiennage, loyer, etc.)</li>
-        </ul>
-      </Accordion>
-      <Button linkProps={routes.createSiteFoncier().link}>Commencer</Button>
-    </WizardFormLayout>
+        <p>
+          Une fois que ce site sera décrit, vous pourrez alors créer un ou plusieurs{" "}
+          <strong>projets d'aménagement</strong> sur ce site.
+        </p>
+        <Accordion label="Documents utiles" className={fr.cx("fr-mb-4w")}>
+          <p>
+            Pour vous aider à saisir les informations liées à votre projet, il peut être utile
+            d'avoir les ressources suivantes :
+          </p>
+          <ul>
+            <li>Plan cadastral du site</li>
+            <li>Acte de vente</li>
+            <li>
+              Logiciel de cartographie satellite (exemple:{" "}
+              <a href="https://www.openstreetmap.fr/" rel="noopener noreferrer" target="_blank">
+                OpenStreetMap
+              </a>
+              )
+            </li>
+            <li>Photographies</li>
+            <li>
+              Carte des anciens sites industriels et de services (
+              <a
+                href="https://infoterre.brgm.fr/viewer/MainTileForward.do"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                https://infoterre.brgm.fr/viewer/MainTileForward.do
+              </a>
+              )
+            </li>
+            <li>Rapports de diagnostics de pollution ou plan de gestion</li>
+            <li>Compte de résultat ou données économiques du site (gardiennage, loyer, etc.)</li>
+          </ul>
+        </Accordion>
+        <Button linkProps={routes.createSiteFoncier().link}>Commencer</Button>
+      </WizardFormLayout>
+    </section>
   );
 }
 
