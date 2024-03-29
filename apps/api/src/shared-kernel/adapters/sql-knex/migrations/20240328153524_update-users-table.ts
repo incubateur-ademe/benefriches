7fd286c620527fc61e7a41677a38dba5e8cfe13d
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-03-29 16:15:21 +0100

    fix(api): rename old users table instead of altering it

--- apps/api/src/shared-kernel/adapters/sql-knex/migrations/20240328153524_update-users-table.ts
+++ apps/api/src/shared-kernel/adapters/sql-knex/migrations/20240328153524_update-users-table.ts
@@ -1,7 +1,10 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.renameTable("users", "users_deprecated");
   await knex.schema.table("users", function (t) {
+    t.uuid("id").primary();
+    t.string("email").notNullable();
     t.string("firstname");
     t.string("lastname");
     t.string("structure_type");
@@ -10,20 +13,10 @@ export async function up(knex: Knex): Promise<void> {
     t.timestamp("personal_data_storage_consented_at").notNullable();
     t.timestamp("personal_data_analytics_use_consented_at");
     t.timestamp("personal_data_communication_use_consented_at");
-    t.dropColumn("password");
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.table("users", function (t) {
-    t.dropColumn("firstname");
-    t.dropColumn("lastname");
-    t.dropColumn("structure_type");
-    t.dropColumn("structure_name");
-    t.dropColumn("created_at");
-    t.dropColumn("personal_data_storage_consented_at");
-    t.dropColumn("personal_data_analytics_use_consented_at");
-    t.dropColumn("personal_data_communication_use_consented_at");
-    t.string("password").notNullable();
-  });
+  await knex.schema.dropTable("users");
+  await knex.schema.renameTable("users_deprecated", "users");
 }
