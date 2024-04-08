5c7b2fafbc6e32e2143a022094569ccdb8bdb5dc
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-04-08 15:34:40 +0200

    feat(web): make impacts chart card titles smaller

--- apps/web/src/features/projects/views/project-impacts-page/ImpactChartCard.tsx
+++ apps/web/src/features/projects/views/project-impacts-page/ImpactChartCard.tsx
@@ -7,6 +7,7 @@ type Props = {
 };
 
 const ImpactCard = ({ title, children, onTitleClick }: Props) => {
+  const titleClassName = `fr-text--sm ${onTitleClick ? "tw-cursor-pointer hover:tw-underline" : ""}`;
   return (
     <figure
       style={{
@@ -16,14 +17,9 @@ const ImpactCard = ({ title, children, onTitleClick }: Props) => {
       }}
       className="tw-flex tw-flex-col fr-py-2w fr-px-3w fr-m-0"
     >
-      {onTitleClick ? (
-        <strong className="tw-cursor-pointer hover:tw-underline" onClick={onTitleClick}>
-          {title}
-        </strong>
-      ) : (
-        <strong>{title}</strong>
-      )}
-
+      <strong className={titleClassName} onClick={onTitleClick}>
+        {title}
+      </strong>
       <div className="tw-flex tw-flex-col tw-grow tw-justify-center">{children}</div>
     </figure>
   );
