0850fa5954fdc5fa00fb182b9ee9e86d3e76b87f
Claire Dhénin <25612600+cdhenin@users.noreply.github.com>
2024-05-14 16:41:21 +0200

    feat(api): assign future operator value to developer as default value in migration

--- apps/api/src/shared-kernel/adapters/sql-knex/migrations/20240429161246_add-developer-to-reconversion-project-development-plans-table.ts
+++ apps/api/src/shared-kernel/adapters/sql-knex/migrations/20240429161246_add-developer-to-reconversion-project-development-plans-table.ts
@@ -5,6 +5,33 @@ export async function up(knex: Knex): Promise<void> {
     table.string("developer_name");
     table.string("developer_structure_type");
   });
+
+  const developmentPlans = (await knex
+    .from("reconversion_project_development_plans")
+    .select(
+      "reconversion_projects.future_operator_name",
+      "reconversion_projects.future_operator_structure_type",
+      "reconversion_project_development_plans.id",
+    )
+    .innerJoin(
+      "reconversion_projects",
+      "reconversion_projects.id",
+      "=",
+      "reconversion_project_development_plans.reconversion_project_id",
+    )) as { future_operator_name?: string; future_operator_structure_type?: string; id: string }[];
+
+  if (developmentPlans.length > 0) {
+    await Promise.all(
+      developmentPlans.map((developmentPlan) => {
+        return knex("reconversion_project_development_plans")
+          .update({
+            developer_name: developmentPlan.future_operator_name,
+            developer_structure_type: developmentPlan.future_operator_structure_type,
+          })
+          .where("id", developmentPlan.id);
+      }),
+    );
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
