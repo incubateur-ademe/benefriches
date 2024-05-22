3f09745ed0d6607e79aaff9107a6d7ce77b0c2fd
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-05-22 15:12:27 +0200

    feat(web): update layout on site creation introduction page

--- apps/web/src/features/create-site/views/introduction/CreateSiteIntroductionPage.tsx
+++ apps/web/src/features/create-site/views/introduction/CreateSiteIntroductionPage.tsx
@@ -5,7 +5,6 @@ import { resetState } from "../../application/createSite.reducer";
 
 import { routes } from "@/app/views/router";
 import { useAppDispatch } from "@/shared/views/hooks/store.hooks";
-import WizardFormLayout from "@/shared/views/layout/WizardFormLayout/WizardFormLayout";
 
 function CreateSiteIntroductionPage() {
   const dispatch = useAppDispatch();
@@ -16,17 +15,20 @@ function CreateSiteIntroductionPage() {
 
   return (
     <section className={fr.cx("fr-container", "fr-py-4w")}>
-      <WizardFormLayout title="Tout commence sur un site">
-        <p>
+      <section className="tw-py-7 lg:tw-px-[200px]">
+        <div className="tw-text-[80px] tw-mb-10 tw-ml-[-18px] tw-leading-none">📍</div>
+        <h2 className="tw-mb-10">Tout commence sur un site</h2>
+        <p className="tw-text-xl tw-mb-10">
           Nous allons d'abord parler du <strong>site existant</strong> : la nature du site, la
           typologie de ses sols, les dépenses associées, etc.
-        </p>
-        <p>
+          <br />
           Une fois que ce site sera décrit, vous pourrez alors créer un ou plusieurs{" "}
           <strong>projets d'aménagement</strong> sur ce site.
         </p>
-        <Button linkProps={routes.createSiteFoncier().link}>Commencer</Button>
-      </WizardFormLayout>
+        <Button size="large" linkProps={routes.createSiteFoncier().link}>
+          Commencer
+        </Button>
+      </section>
     </section>
   );
 }
