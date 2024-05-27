8d8a652349a72b72306b3c4a37a38440a914626d
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-05-27 18:51:33 +0200

    feat(web): use EditorialPageLayout in site soils introduction step

--- apps/web/src/features/create-site/views/soils/soils-introduction/SoilsIntroduction.tsx
+++ apps/web/src/features/create-site/views/soils/soils-introduction/SoilsIntroduction.tsx
@@ -1,5 +1,10 @@
 import BackNextButtonsGroup from "@/shared/views/components/BackNextButtons/BackNextButtons";
-import WizardFormLayout from "@/shared/views/layout/WizardFormLayout/WizardFormLayout";
+import {
+  EditorialPageIcon,
+  EditorialPageLayout,
+  EditorialPageText,
+  EditorialPageTitle,
+} from "@/shared/views/layout/EditorialPageLayout";
 
 type Props = {
   onNext: () => void;
@@ -8,19 +13,18 @@ type Props = {
 };
 
 const SiteSoilsIntroduction = ({ isFriche, onNext, onBack }: Props) => {
-  const title = (
-    <>
-      ⛳️ <br /> Parlons d'abord des sols qui existent sur {isFriche ? "la friche" : "le site"}
-    </>
-  );
   return (
-    <WizardFormLayout title={title}>
-      <p>
+    <EditorialPageLayout>
+      <EditorialPageIcon>⛳️</EditorialPageIcon>
+      <EditorialPageTitle>
+        Parlons d'abord des sols qui existent sur {isFriche ? "la friche" : "le site"}
+      </EditorialPageTitle>
+      <EditorialPageText>
         Nous avons besoin de connaître leur typologie, leur occupation et les superficies
         correspondantes.
-      </p>
+      </EditorialPageText>
       <BackNextButtonsGroup onBack={onBack} onNext={onNext} />
-    </WizardFormLayout>
+    </EditorialPageLayout>
   );
 };
 
