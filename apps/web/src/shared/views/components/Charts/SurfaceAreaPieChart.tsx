cc95ff1b24afdf333fa8450c08bbddc314ef6508
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-05-29 11:48:33 +0200

    feat(web): set charts data-label font-size to 14px

--- apps/web/src/shared/views/components/Charts/SurfaceAreaPieChart.tsx
+++ apps/web/src/shared/views/components/Charts/SurfaceAreaPieChart.tsx
@@ -37,7 +37,7 @@ const SurfaceAreaPieChart = ({ soilsDistribution }: Props) => {
     plotOptions: { pie: { cursor: "pointer" } },
     series: [
       {
-        name: "Stockage carbone",
+        name: "Superficie",
         type: "pie",
         data,
       },
