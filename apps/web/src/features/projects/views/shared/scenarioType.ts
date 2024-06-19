70f257e136adf42c068947f7db4746fdb8ccb474
Claire Dhénin <25612600+cdhenin@users.noreply.github.com>
2024-06-19 12:00:21 +0200

    refacto: rename BUILDINGS to MIXED_USE_NEIGHBOURHOOD

--- apps/web/src/features/projects/views/shared/scenarioType.ts
+++ apps/web/src/features/projects/views/shared/scenarioType.ts
@@ -2,7 +2,7 @@ export const getScenarioPictoUrl = (type: string) => {
   switch (type) {
     case "PHOTOVOLTAIC_POWER_PLANT":
       return "/img/pictograms/renewable-energy/photovoltaic.svg";
-    case "BUILDINGS":
+    case "MIXED_USE_NEIGHBOURHOOD":
       return "/img/pictograms/development-plans/mixed-used-neighborhood.svg";
     default:
       return undefined;
