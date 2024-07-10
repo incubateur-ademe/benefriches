c2206cac46c5dc479e7139ef8ace86cd3edb31a9
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-07-10 18:11:58 +0200

    feat(web): update layout and text on 'about project impacts' modal

--- apps/web/src/features/projects/views/project-impacts-page/AboutImpactsModal/AboutImpactsModal.tsx
+++ apps/web/src/features/projects/views/project-impacts-page/AboutImpactsModal/AboutImpactsModal.tsx
@@ -31,6 +31,10 @@ function StopDisplayingModalCheckbox({ checked, onChange }: StopDisplayingModalC
   );
 }
 
+const SectionTitle = ({ children }: { children: string }) => {
+  return <h3 className="tw-text-base tw-mb-1">{children}</h3>;
+};
+
 type Props = {
   shouldOpenModal: boolean;
   setShouldNotDisplayAgain: (value: boolean) => void;
@@ -57,61 +61,52 @@ function AboutImpactsModal({ shouldOpenModal, setShouldNotDisplayAgain }: Props)
         },
       ]}
     >
-      <ul>
-        <li>
-          <strong>Les indicateurs sont calculés à partir de données sourcées</strong>
-          <p>
-            Vous trouverez dans cette page des indicateurs d'impacts économiques, sociaux et
-            environnementaux (voire certains combinant les 2 premiers aspects), calculés à partir
-            des données que vous avez saisies (ex : les surfaces des différents types de sol ou
-            encore la quantité d'énergie renouvelable qui sera produite annuellement) et :
-          </p>
-          <ul>
-            <li>
-              de valeurs de référence compilées dans Bénéfriches à partir de publications sourcées
-              (INSEE, France Stratégie, AGRESTE, ADEME, publications scientifiques, etc.)
-            </li>
-            <li>
-              et/ou de bases de données accessible en{" "}
-              <ExternalLink href="https://www.data.gouv.fr/fr/">opendata</ExternalLink> (dvf, etc.)
-            </li>
-            <li>
-              et/ou d'hypothèses contextualisées (ex : temps de déplacement gagné par les habitants
-              du fait de la création d'une nouvelle centralité à partir des données de{" "}
-              <ExternalLink href="https://www.observatoire-des-territoires.gouv.fr/">
-                l'Observatoire des territoires
-              </ExternalLink>
-              ).
-            </li>
-          </ul>
-        </li>
-        <li>
-          <strong>
-            Les indicateurs monétaires ne tiennent pas encore compte du coefficient d'actualisation
-          </strong>
-        </li>
-        <li>
-          <strong>Les indicateurs affichent la valeur différentielle</strong>
-          <p>
-            Dans cette page, les indicateurs monétarisés ne montrent pas une comparaison ni un état
-            final mais un différentiel. C'est-à-dire la valeur issue de la monétarisation des
-            impacts, en plus (bénéfices) ou en moins (dommages) que le projet va générer par rapport
-            à la conservation du site dans son état.
-          </p>
-        </li>
-        <li>
-          <strong>Le bénéfice de la vente du site n'apparaît pas dans les impacts</strong>
-          <p>
-            La cession foncière, au bénéfice de l'actuel propriétaire, s'il y a eu vente du site,
-            n'apparaît pas dans les impacts. En revanche, l'acquisition foncière, si elle est à la
-            charge de l'aménageur, est bien prise en compte. Ceci est un parti pris de Bénéfriches.
-          </p>
-        </li>
-        <li>
-          <strong>Cette page est consultable à tout moment.</strong>
-          <p>Vous pouvez la retrouver dans votre espace “Mes projets”</p>
-        </li>
-      </ul>
+      <section className="tw-mb-4">
+        <SectionTitle>📊 Les indicateurs sont calculés à partir de données sourcées</SectionTitle>
+        <p className="tw-mb-2">
+          Vous trouverez dans cette page des indicateurs d'impacts économiques, sociaux et
+          environnementaux (voire certains combinant les 2 premiers aspects), calculés à partir des
+          données que vous avez saisies (ex : les surfaces des différents types de sol ou encore la
+          quantité d'énergie renouvelable qui sera produite annuellement) et :
+        </p>
+        <ul>
+          <li>
+            de valeurs de référence compilées dans Bénéfriches à partir de publications sourcées
+            (INSEE, France Stratégie, AGRESTE, ADEME, publications scientifiques, etc.)
+          </li>
+          <li>
+            et/ou de bases de données accessible en{" "}
+            <ExternalLink href="https://www.data.gouv.fr/fr/">opendata</ExternalLink> (dvf, etc.)
+          </li>
+          <li>
+            et/ou d'hypothèses contextualisées (ex : temps de déplacement gagné par les habitants du
+            fait de la création d'une nouvelle centralité à partir des données de l'
+            <ExternalLink href="https://www.observatoire-des-territoires.gouv.fr/">
+              Observatoire des territoires
+            </ExternalLink>
+            ).
+          </li>
+        </ul>
+      </section>
+      <section className="tw-mb-4">
+        <SectionTitle>
+          💰 Les indicateurs monétaires ne tiennent pas encore compte du coefficient d'actualisation
+        </SectionTitle>
+        <p>Ce sera le cas dans une prochaine version de l'outil.</p>
+      </section>
+      <section className="tw-mb-4">
+        <SectionTitle>⚖️ Les indicateurs affichent la valeur différentielle</SectionTitle>
+        <p>
+          Dans cette page, les indicateurs monétarisés ne montrent pas une comparaison ni un état
+          final mais un différentiel. C'est-à-dire la valeur issue de la monétarisation des impacts,
+          en plus (bénéfices) ou en moins (dommages) que le projet va générer par rapport à la
+          conservation du site dans son état.
+        </p>
+      </section>
+      <section>
+        <SectionTitle>💾 Cette page est consultable à tout moment</SectionTitle>
+        <p>Vous pouvez la retrouver dans votre espace "Mes projets"</p>
+      </section>
       <StopDisplayingModalCheckbox
         checked={shouldStopDisplayingModal}
         onChange={() => {
