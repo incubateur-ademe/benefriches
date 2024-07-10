46da61ba16825270a1e1815674c9649d73bf4a30
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-07-10 18:11:19 +0200

    feat(web): add definition text on site full time jobs form

--- apps/web/src/features/create-site/views/site-management/full-time-jobs/SiteFullTimeJobsInvolvedForm.tsx
+++ apps/web/src/features/create-site/views/site-management/full-time-jobs/SiteFullTimeJobsInvolvedForm.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 
 import BackNextButtonsGroup from "@/shared/views/components/BackNextButtons/BackNextButtons";
 import NumericInput from "@/shared/views/components/form/NumericInput/NumericInput";
+import FormDefinition from "@/shared/views/layout/WizardFormLayout/FormDefinition";
 import WizardFormLayout from "@/shared/views/layout/WizardFormLayout/WizardFormLayout";
 
 type Props = {
@@ -17,7 +18,18 @@ function SiteFullTimeJobsInvolvedForm({ onSubmit, onBack }: Props) {
   const { control, handleSubmit } = useForm<FormValues>();
 
   return (
-    <WizardFormLayout title="Combien d'emplois équivalent temps-plein sont mobilisés sur le site ?">
+    <WizardFormLayout
+      title="Combien d'emplois équivalent temps-plein sont mobilisés sur le site ?"
+      instructions={
+        <FormDefinition hideDivider>
+          <p>
+            Le site peut nécessiter la mobilisation de personnel (interne ou via des prestations)
+            pour empêcher des intrusions (ex : gardiennage) ou maintenir les bâtiments ou les accès
+            en état (intervention sur des clôtures, portails, ouvertures, etc.).
+          </p>
+        </FormDefinition>
+      }
+    >
       <form onSubmit={handleSubmit(onSubmit)}>
         <NumericInput
           name="fullTimeJobsInvolved"
