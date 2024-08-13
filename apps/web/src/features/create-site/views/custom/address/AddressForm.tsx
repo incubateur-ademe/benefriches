0c1dd35b8c269cf4d9febb02f54edf45f5913c0e
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-08-13 11:59:28 +0200

    feat(web): add explanations on address form

--- apps/web/src/features/create-site/views/custom/address/AddressForm.tsx
+++ apps/web/src/features/create-site/views/custom/address/AddressForm.tsx
@@ -35,11 +35,30 @@ function SiteAddressForm({ onSubmit, isFriche, onBack }: Props) {
         title={title}
         instructions={
           <FormInfo>
-            <p>Bénéfriches a besoin de connaître l'adresse du site.</p>
             <p>
               Si vous ne connaissez pas l'adresse exacte, entrez simplement le nom de la commune ou
               son code postal.
             </p>
+            <p className="tw-text-sm tw-m-0">
+              L'adresse sert à positionner le site dans son environnement local, ce qui est utile
+              pour&nbsp;:
+            </p>
+            <ul>
+              <li>Connaître le type d'environnement (résidentiel, industriel, naturel, etc.)</li>
+              <li>
+                Déterminer des paramètres socio-économiques locaux (marché foncier et immobilier,
+                densité de population, d'emplois, etc.)
+              </li>
+              <li>
+                Identifier les éventuels services de proximité (loisirs, culturels, éducatifs, etc.)
+              </li>
+              <li>Identifier la proximité d'espaces naturels, agricoles ou forestiers</li>
+              <li>
+                Identifier la proximité de parcs urbains ou d'espaces de nature en ville Autant
+                d'informations qui permettent à Bénéfriches de quantifier des indicateurs d'impacts
+                socio-économiques.
+              </li>
+            </ul>
           </FormInfo>
         }
       >
