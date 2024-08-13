4c3d3b9863ba55fdef6826c72231a7a416efda74
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-08-13 16:58:43 +0200

    fix(web): impact charts content was jumping when hovered because of border

--- apps/web/src/features/projects/views/project-page/impacts/charts-view/ImpactsChartsSection.tsx
+++ apps/web/src/features/projects/views/project-page/impacts/charts-view/ImpactsChartsSection.tsx
@@ -21,11 +21,14 @@ const ImpactsChartsSection = ({ title, children, onClick }: Props) => {
         "tw-h-full",
         "tw-bg-white",
         "dark:tw-bg-black",
-        onClick && ["tw-cursor-pointer", "hover:tw-border-solid", "hover:tw-border"],
+        "tw-border",
+        "tw-border-solid",
+        "tw-border-transparent",
+        onClick && ["tw-cursor-pointer", "hover:tw-border-current"],
       )}
       onClick={onClick}
     >
-      <h3 className={classNames("tw-text-2xl")}>{title}</h3>
+      <h3 className="tw-text-2xl">{title}</h3>
       <div className="tw-flex tw-flex-col tw-grow tw-justify-center">{children}</div>
     </section>
   );
