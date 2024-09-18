a2f13fe417c2f836df167231d6abdc96d3d806ce
Claire Dhénin <25612600+cdhenin@users.noreply.github.com>
2024-09-18 15:41:24 +0200

    fix(web): wrong tooltip sign prefix for avoided co2eq emissions

--- apps/web/src/features/projects/views/project-page/impacts/synthesis-view/impacts/AvoidedCo2eqEmissions.tsx
+++ apps/web/src/features/projects/views/project-page/impacts/synthesis-view/impacts/AvoidedCo2eqEmissions.tsx
@@ -11,15 +11,21 @@ type Props = {
 };
 
 const ImpactSynthesisAvoidedCo2eqEmissions = ({ value, isSuccess, ...props }: Props) => {
-  const frenchPersonAnnualEquivalent = formatPerFrenchPersonAnnualEquivalent(
-    getCo2EqEmissionsTonsInAverageFrenchAnnualEmissionsPerPerson(value),
+  const co2eqValue = Math.abs(value);
+  const co2eqValueText = formatCO2Impact(co2eqValue, { withSignPrefix: false });
+  const frenchPersonAnnualEquivalent =
+    getCo2EqEmissionsTonsInAverageFrenchAnnualEmissionsPerPerson(co2eqValue);
+
+  const frenchPersonAnnualEquivalentText = formatPerFrenchPersonAnnualEquivalent(
+    frenchPersonAnnualEquivalent,
   );
+
   if (isSuccess) {
     return (
       <ImpactSyntheticCard
         {...props}
         type="success"
-        tooltipText={`${formatCO2Impact(value)} de CO2-éq évitées, soit les émissions de ${frenchPersonAnnualEquivalent} français pendant 1 an`}
+        tooltipText={`${co2eqValueText} de CO2-éq évitées, soit les émissions de ${frenchPersonAnnualEquivalentText} français pendant 1 an`}
         text="- d’émissions de CO2&nbsp;☁️"
       />
     );
@@ -29,7 +35,7 @@ const ImpactSynthesisAvoidedCo2eqEmissions = ({ value, isSuccess, ...props }: Pr
     <ImpactSyntheticCard
       {...props}
       type="error"
-      tooltipText={`${formatCO2Impact(value)} de CO2-éq émises, soit les émissions de ${frenchPersonAnnualEquivalent} français pendant 1 an`}
+      tooltipText={`${co2eqValueText} de CO2-éq émises, soit les émissions de ${frenchPersonAnnualEquivalentText} français pendant 1 an`}
       text="+ d’émissions de CO2&nbsp;☁️"
     />
   );
