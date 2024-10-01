e5c324ccf30bd8c844586ca04c40462976529354
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-10-01 10:42:36 +0200

    feat(web): make site/project features data line more responsive

--- apps/web/src/shared/views/components/FeaturesList/FeaturesListScheduleDates.tsx
+++ apps/web/src/shared/views/components/FeaturesList/FeaturesListScheduleDates.tsx
@@ -13,7 +13,7 @@ export default function ScheduleDates({ startDateString, endDateString }: Schedu
       <strong>
         {startDate.toLocaleDateString()} ➔ {endDate.toLocaleDateString()}
       </strong>{" "}
-      ({getFormattedDuration(startDate, endDate)})
+      <span className="tw-text-nowrap">({getFormattedDuration(startDate, endDate)})</span>
     </span>
   );
 }
