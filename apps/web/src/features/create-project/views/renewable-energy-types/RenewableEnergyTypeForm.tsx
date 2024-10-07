e82a442afa1bee3cdab6a13b924b3fd7c9af7a09
Claire Dhénin <claire.dhenin@nebulae.co>
2024-10-07 15:34:52 +0200

    feat(web): update project type and EnR type tile layout in form to prevent scroll to validate

--- apps/web/src/features/create-project/views/renewable-energy-types/RenewableEnergyTypeForm.tsx
+++ apps/web/src/features/create-project/views/renewable-energy-types/RenewableEnergyTypeForm.tsx
@@ -28,40 +28,46 @@ function RenewableEnergyTypesForm({ onSubmit, onBack }: Props) {
   const validationError = formState.errors.renewableEnergyType;
 
   return (
-    <WizardFormLayout title="Quel système d'EnR souhaitez-vous installer ?">
+    <WizardFormLayout title="Quel système d'EnR souhaitez-vous installer&nbsp;?">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className={fr.cx("fr-mb-5w")}>
-          <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
-            {typedObjectKeys(options).map((renewableEnergy) => {
-              return (
-                <div className={fr.cx("fr-col-12", "fr-col-sm-6")} key={renewableEnergy}>
-                  <Controller
-                    control={control}
-                    name="renewableEnergyType"
-                    rules={{
-                      required: "Veuillez sélectionner au moins un type d'énergie renouvelable.",
-                    }}
-                    render={({ field }) => {
-                      const isSelected = field.value === renewableEnergy;
-                      return (
-                        <RenewableEnergyTile
-                          renewableEnergy={renewableEnergy}
-                          disabled={options[renewableEnergy].disabled}
-                          isSelected={isSelected}
-                          onSelect={() => {
-                            field.onChange(renewableEnergy);
-                          }}
-                        />
-                      );
-                    }}
-                  />
-                </div>
-              );
-            })}
+        <div className="tw-grid tw-grid-cols-[repeat(auto-fill,_357px)] tw-gap-x-4">
+          {typedObjectKeys(options).map((renewableEnergy) => {
+            return (
+              <div className="tw-mb-4" key={renewableEnergy}>
+                <Controller
+                  control={control}
+                  name="renewableEnergyType"
+                  rules={{
+                    required: "Veuillez sélectionner au moins un type d'énergie renouvelable.",
+                  }}
+                  render={({ field }) => {
+                    const isSelected = field.value === renewableEnergy;
+                    return (
+                      <RenewableEnergyTile
+                        renewableEnergy={renewableEnergy}
+                        disabled={options[renewableEnergy].disabled}
+                        isSelected={isSelected}
+                        onSelect={() => {
+                          field.onChange(renewableEnergy);
+                        }}
+                      />
+                    );
+                  }}
+                />
+              </div>
+            );
+          })}
+          <div className="tw-row-start-5 tw-col-start-1 tw-col-end-[-1]">
+            {validationError && (
+              <p className={fr.cx("fr-error-text", "fr-mb-2w")}>{validationError.message}</p>
+            )}
+            <BackNextButtonsGroup
+              onBack={onBack}
+              nextLabel="Valider"
+              disabled={!formState.isValid}
+            />
           </div>
-          {validationError && <p className={fr.cx("fr-error-text")}>{validationError.message}</p>}
         </div>
-        <BackNextButtonsGroup onBack={onBack} nextLabel="Valider" disabled={!formState.isValid} />
       </form>
     </WizardFormLayout>
   );
