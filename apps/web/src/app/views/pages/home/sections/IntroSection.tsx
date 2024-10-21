b81f67c52121c4f66ab133f8c8923f6225f30f63
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-10-21 16:33:57 +0200

    feat(web): improve responsive layout for home page introduction

--- apps/web/src/app/views/pages/home/sections/IntroSection.tsx
+++ apps/web/src/app/views/pages/home/sections/IntroSection.tsx
@@ -1,29 +1,35 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import Button from "@codegouvfr/react-dsfr/Button";
 
+import classNames from "@/shared/views/clsx";
+
 export default function IntroSection() {
   return (
-    <section className={fr.cx("fr-container", "fr-py-10w", "fr-pr-md-0")}>
-      <div className={fr.cx("fr-grid-row")}>
-        <div className={fr.cx("fr-col-12", "fr-col-md-7", "fr-pr-md-6w", "fr-m-auto")}>
-          <h1>Bénéfriches, l'outil qui calcule la valeur réelle de votre projet d'aménagement</h1>
-          <p className={fr.cx("fr-text--xl")}>
-            Bénéfriches quantifie et monétarise les impacts{" "}
-            <strong>environnementaux, sociaux et économiques</strong> d'un projet d'aménagement, sur{" "}
-            <strong>friche</strong> ou en <strong>extension urbaine</strong>.
-          </p>
-          <Button priority="primary" linkProps={{ href: "#cta-section" }}>
-            Calculer les impacts de votre projet
-          </Button>
+    <div className="tw-overflow-hidden">
+      <section className={classNames("fr-container tw-py-8", "md:max-lg:tw-px-32", "lg:tw-py-20")}>
+        <div className="tw-flex tw-flex-col lg:tw-flex-row">
+          <div className="lg:tw-flex-[7] lg:tw-margin-auto">
+            <h1>Bénéfriches, l'outil qui calcule la valeur réelle de votre projet d'aménagement</h1>
+            <p className={fr.cx("fr-text--xl")}>
+              Bénéfriches quantifie et monétarise les impacts{" "}
+              <strong>environnementaux, sociaux et économiques</strong> d'un projet d'aménagement,
+              sur <strong>friche</strong> ou en <strong>extension urbaine</strong>.
+            </p>
+            <Button priority="primary" linkProps={{ href: "#cta-section" }}>
+              Calculer les impacts de votre projet
+            </Button>
+          </div>
+          <div
+            className={classNames("tw-relative tw-pt-10", "lg:tw-flex-[5] lg:tw-pl-12 lg:tw-pt-0")}
+          >
+            <img
+              src="/img/home-intro.svg"
+              className="tw-w-full lg:tw-w-auto lg:tw-absolute"
+              alt="Capture d'écran de Bénéfriches : page impacts d'un projet d'aménagement"
+            />
+          </div>
         </div>
-        <div className={fr.cx("fr-col-12", "fr-col-md-5", "fr-pt-10v", "fr-pt-md-0")}>
-          <img
-            src="/img/home-intro.svg"
-            className="tw-w-full md:tw-w-auto"
-            alt="capture d’écran de l’outil Bénéfriches"
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
