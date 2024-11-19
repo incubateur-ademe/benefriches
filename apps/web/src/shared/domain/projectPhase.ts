bf7c16e8d2265049c9b110a4996e3b393b44b124
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2024-11-19 11:38:34 +0100

    feat(web): update hint text for project phase 'planning'

--- apps/web/src/shared/domain/projectPhase.ts
+++ apps/web/src/shared/domain/projectPhase.ts
@@ -21,6 +21,8 @@ export const getHintTextForProjectPhase = (projectPhase: ProjectPhase): string |
   switch (projectPhase) {
     case "setup":
       return "Recherche de site, faisabilité, concertation locale, éude d'impact, dépôt de permis";
+    case "planning":
+      return "Au sens de la loi sur la maîtrise d'ouvrage publique, sont définis dans le cadre du  programme : les objectifs de l'opération et les besoins qu'elle doit satisfaire ainsi que les contraintes et exigences de qualité sociale, urbanistique, architecturale, fonctionnelle, technique et économique, d'insertion dans le paysage et de protection de l'environnement, relatives à la réalisation et à l'utilisation de l'ouvrage.";
     default:
       return null;
   }
