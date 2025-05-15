cbc9af5fcb1af9f80007b3e0e85706bafe25087a
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-05-15 15:12:18 +0200

    feat(api): save link between benefriches user and external identity provider

---
+++ apps/api/src/auth/adapters/external-user-identities-repository/ExternalUserIdentitiesRepository.ts
@@ -0,0 +1,20 @@
+export const EXTERNAL_USER_IDENTITIES_REPOSITORY_INJECTION_TOKEN = Symbol(
+  "ExternalUserIdentityRepository",
+);
+
+export type ExternalUserIdentity = {
+  id: string;
+  userId: string;
+  provider: string;
+  providerUserId: string;
+  createdAt: Date;
+};
+
+export interface ExternalUserIdentityRepository {
+  save(userIdentity: ExternalUserIdentity): Promise<void>;
+
+  findByProviderUserId(
+    provider: string,
+    providerUserId: string,
+  ): Promise<ExternalUserIdentity | undefined>;
+}
