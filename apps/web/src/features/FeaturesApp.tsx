7157e844945789c124baa7d2ad32ce86c0592e4f
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-05-19 11:22:58 +0200

    feat(web): fetch current user on root page component

--- apps/web/src/features/FeaturesApp.tsx
+++ apps/web/src/features/FeaturesApp.tsx
@@ -1,13 +1,11 @@
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense } from "react";
 import { createGroup } from "type-route";
 
-import { initCurrentUser } from "@/features/onboarding/core/initCurrentUser.action";
 import MatomoContainer from "@/shared/views/MatomoContainer";
 import NotFoundScreen from "@/shared/views/components/NotFound/NotFound";
 import RequireAuthenticatedUser from "@/shared/views/components/RequireAuthenticatedUser/RequireAuthenticatedUser";
 import LoadingSpinner from "@/shared/views/components/Spinner/LoadingSpinner";
 import { BENEFRICHES_ENV } from "@/shared/views/envVars";
-import { useAppDispatch } from "@/shared/views/hooks/store.hooks";
 import HeaderFooterLayout from "@/shared/views/layout/HeaderFooterLayout/HeaderFooterLayout";
 import SidebarContainerLayout from "@/shared/views/layout/SidebarLayout/SidebarContainerLayout";
 import { routes, useRoute } from "@/shared/views/router";
@@ -40,11 +38,6 @@ const onBoardingGroup = createGroup([
 
 function FeaturesApp() {
   const route = useRoute();
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    void dispatch(initCurrentUser());
-  }, [dispatch]);
 
   if (formsLayoutGroup.has(route)) {
     return (
