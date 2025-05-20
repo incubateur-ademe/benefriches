820b9d3434f4ead3941485056315590fcc44bf78
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-05-20 11:42:42 +0200

    feat(web): re-order urban spaces categories options

--- packages/shared/src/reconversion-projects/urban-project/spaces/spaceCategories.ts
+++ packages/shared/src/reconversion-projects/urban-project/spaces/spaceCategories.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const urbanProjectSpacesCategories = z.enum([
   "LIVING_AND_ACTIVITY_SPACES",
-  "PUBLIC_SPACES",
   "GREEN_SPACES",
+  "PUBLIC_SPACES",
   "URBAN_FARM",
   "RENEWABLE_ENERGY_PRODUCTION_PLANT",
   "URBAN_POND_OR_LAKE",
