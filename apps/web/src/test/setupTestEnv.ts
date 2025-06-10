390889f56d96d364821c12baaeb7bf75c7cddb10
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-06-10 12:06:43 +0200

    chore: update external dependencies

--- apps/web/src/test/setupTestEnv.ts
+++ apps/web/src/test/setupTestEnv.ts
@@ -1,3 +1,11 @@
 import "@testing-library/jest-dom/vitest";
 
 (window as Window & typeof globalThis & { _benefriches_env: object })._benefriches_env = {};
+
+// needed because highcharts checks css support
+// see https://github.com/highcharts/highcharts/issues/22910
+vi.stubGlobal("CSS", {
+  supports: vi.fn().mockImplementation(() => {
+    return true;
+  }),
+});
