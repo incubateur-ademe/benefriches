d852a8a7d8324c82268780a577af68124733c668
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-06-17 16:33:02 +0200

    feat(web): update homepage hero image

--- apps/web/src/features/public-pages/home/sections/IntroSection.tsx
+++ apps/web/src/features/public-pages/home/sections/IntroSection.tsx
@@ -7,7 +7,7 @@ export default function IntroSection() {
   return (
     <div className="tw-overflow-hidden">
       <section className={classNames("fr-container tw-py-8", "md:max-lg:tw-px-32", "lg:tw-py-20")}>
-        <div className="tw-flex tw-flex-col lg:tw-flex-row">
+        <div className="tw-flex tw-flex-col lg:tw-flex-row tw-gap-8 lg:tw-gap-4">
           <div className="lg:tw-flex-[7] lg:tw-margin-auto">
             <h1>Bénéfriches, l'outil qui calcule la valeur réelle de votre projet d'aménagement</h1>
             <p className={fr.cx("fr-text--xl")}>
@@ -19,12 +19,10 @@ export default function IntroSection() {
               Calculer les impacts de votre projet
             </Button>
           </div>
-          <div
-            className={classNames("tw-relative tw-pt-10", "lg:tw-flex-[5] lg:tw-pl-12 lg:tw-pt-0")}
-          >
+          <div className="lg:tw-flex-[5]">
             <img
               src="/img/home-hero.png"
-              className="tw-w-full lg:tw-absolute"
+              className="tw-w-full"
               alt="Capture d'écran de Bénéfriches : page impacts d'un projet d'aménagement"
             />
           </div>
