4610b8a8f6b81c81ebcf8493e8b46bff52d427ba
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-06-20 15:43:38 +0200

    feat(web): contextualize creation mode tiles to site nature in site creation

--- apps/web/src/features/create-site/views/create-mode-selection/CreateModeSelectionForm.tsx
+++ apps/web/src/features/create-site/views/create-mode-selection/CreateModeSelectionForm.tsx
@@ -27,26 +27,26 @@ type Option = {
   badgeColor: "blue" | "green-tilleul";
 };
 
-const options: Option[] = [
-  {
-    value: "express",
-    title: "Mode express",
-    description:
-      "Renseignez seulement 3 infos : le type de site, sa commune et sa superficie. Bénéfriches affectera des données par défaut sur la répartition des sols, les dépenses de gestion, etc.",
-    badgeText: "Le plus rapide",
-    badgeColor: "green-tilleul",
-    imgSrc: "/img/pictograms/creation-mode/express-creation.svg",
-  },
-  {
-    value: "custom",
-    title: "Mode personnalisé",
-    description:
-      "Renseignez les infos dont vous disposez : type de site, superficie, adresse, répartition des sols, acteurs, dépenses de gestion, etc. Si certaines infos vous manquent, Bénéfriches vous proposera des données automatiques.",
-    badgeText: "Le plus précis",
-    badgeColor: "blue",
-    imgSrc: "/img/pictograms/creation-mode/custom-creation.svg",
-  },
-] as const satisfies Option[];
+const getOptions = (siteNature: SiteNature): Option[] => {
+  return [
+    {
+      value: "express",
+      title: "Mode express",
+      description: `Renseignez seulement 3 infos : le type de ${siteNature === "FRICHE" ? "friche" : "site"}, sa commune et sa superficie. Bénéfriches affectera des données par défaut sur la répartition des sols, les dépenses de gestion, etc.`,
+      badgeText: "Le plus rapide",
+      badgeColor: "green-tilleul",
+      imgSrc: "/img/pictograms/creation-mode/express-creation.svg",
+    },
+    {
+      value: "custom",
+      title: "Mode personnalisé",
+      description: `Renseignez les infos dont vous disposez : type de ${siteNature === "FRICHE" ? "friche" : "site"}, superficie, adresse, répartition des sols, acteurs, dépenses de gestion, etc. Si certaines infos vous manquent, Bénéfriches vous proposera des données automatiques.`,
+      badgeText: "Le plus précis",
+      badgeColor: "blue",
+      imgSrc: "/img/pictograms/creation-mode/custom-creation.svg",
+    },
+  ] as const satisfies Option[];
+};
 
 const getTitle = (siteNature: SiteNature) => {
   const withSiteNature = (natureLabel: string) =>
@@ -70,7 +70,7 @@ function CreateModeSelectionForm({ siteNature, onSubmit, onBack }: Props) {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="tw-mb-10">
           <div className="tw-grid sm:tw-grid-cols-2 tw-gap-4">
-            {options.map((option) => {
+            {getOptions(siteNature).map((option) => {
               return (
                 <Controller
                   key={option.value}
