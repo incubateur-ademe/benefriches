9190e135d96b5e44a29d16f5ff174ff1d535bc86
Claire Dhénin <claire.dhenin@nebulae.co>
2025-08-20 11:41:06 +0200

    feat(web): add env var to enable urban project custom form with event sourcing and button to disable it from form

--- apps/web/src/features/create-project/views/urban-project/custom-forms-beta/index.tsx
+++ apps/web/src/features/create-project/views/urban-project/custom-forms-beta/index.tsx
@@ -1,10 +1,16 @@
+import { Route } from "type-route";
+
 import { selectCurrentStep } from "@/features/create-project/core/urban-project-event-sourcing/urbanProject.selectors";
 import { useAppSelector } from "@/shared/views/hooks/store.hooks";
+import { routes } from "@/shared/views/router";
 
 import UrbanProjectCustomCreationStepWizardBeta from "./content-wizards/StepWizardBeta";
 
-export default function UrbanProjectCustomCreationStepWizardBetaContainer() {
+type Props = {
+  route: Route<typeof routes.createProject>;
+};
+export default function UrbanProjectCustomCreationStepWizardBetaContainer({ route }: Props) {
   const currentStep = useAppSelector(selectCurrentStep);
 
-  return <UrbanProjectCustomCreationStepWizardBeta currentStep={currentStep} />;
+  return <UrbanProjectCustomCreationStepWizardBeta currentStep={currentStep} route={route} />;
 }
