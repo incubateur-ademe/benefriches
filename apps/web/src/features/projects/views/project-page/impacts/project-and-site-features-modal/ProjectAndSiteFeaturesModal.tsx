7c55b19f4e83b6f91b1d0d1928c969fedb95b115
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-11-03 12:06:07 +0100

    refactor(web): use common Accordion component in FAQ and site/project features modal

--- apps/web/src/features/projects/views/project-page/impacts/project-and-site-features-modal/ProjectAndSiteFeaturesModal.tsx
+++ apps/web/src/features/projects/views/project-page/impacts/project-and-site-features-modal/ProjectAndSiteFeaturesModal.tsx
@@ -1,51 +1,11 @@
-import Button from "@codegouvfr/react-dsfr/Button";
-import { useState } from "react";
-
 import { ProjectFeatures } from "@/features/projects/domain/projects.types";
 import { SiteFeatures } from "@/features/site-features/core/siteFeatures";
 import SiteFeaturesList from "@/features/site-features/views/SiteFeaturesList";
-import classNames from "@/shared/views/clsx";
+import Accordion from "@/shared/views/components/Accordion/Accordion";
 import Dialog from "@/shared/views/components/Dialog/A11yDialog";
 import LoadingSpinner from "@/shared/views/components/Spinner/LoadingSpinner";
 
 import ProjectFeaturesView from "../../features/ProjectFeaturesView";
-
-type AccordionProps = {
-  onClick: () => void;
-  label: string;
-  children: React.ReactNode;
-  isOpen: boolean;
-};
-
-function Accordion({ onClick, isOpen, label, children }: AccordionProps) {
-  return (
-    <div>
-      <div
-        className={classNames(
-          "py-2 px-4 mb-4",
-          "rounded-sm border border-solid border-transparent",
-          "bg-impacts-main dark:bg-black",
-          "cursor-pointer",
-          "transition ease-in-out duration-500",
-          "hover:border-grey-dark hover:dark:border-white",
-          "flex items-center gap-2",
-        )}
-        onClick={onClick}
-      >
-        <Button
-          className={classNames("text-black dark:text-white")}
-          iconId={isOpen ? "fr-icon-arrow-up-s-fill" : "fr-icon-arrow-down-s-fill"}
-          onClick={onClick}
-          size="small"
-          priority="tertiary no outline"
-          title={isOpen ? "Fermer la section" : "Afficher la section"}
-        />
-        <h2 className="text-lg mb-0">{label}</h2>
-      </div>
-      {isOpen && <div className="px-4">{children}</div>}
-    </div>
-  );
-}
 
 type Props = {
   projectFeaturesData?: ProjectFeatures;
@@ -58,32 +18,14 @@ const ProjectAndSiteFeaturesModal = ({
   siteFeaturesData,
   dialogId,
 }: Props) => {
-  const [isProjectFeaturesOpen, setProjectFeaturesOpen] = useState(true);
-  const [isSiteFeaturesOpen, setSiteFeaturesOpen] = useState(true);
-
-  const toggleProjectFeatures = () => {
-    setProjectFeaturesOpen((prev) => !prev);
-  };
-  const toggleSiteFeatures = () => {
-    setSiteFeaturesOpen((prev) => !prev);
-  };
-
   return (
     <Dialog dialogId={dialogId} title="Données du site et du projet" size="medium">
       {projectFeaturesData && siteFeaturesData ? (
         <div>
-          <Accordion
-            label={`Données du site "${siteFeaturesData.name}"`}
-            isOpen={isSiteFeaturesOpen}
-            onClick={toggleSiteFeatures}
-          >
+          <Accordion label={`Données du site "${siteFeaturesData.name}"`} defaultOpen>
             <SiteFeaturesList {...siteFeaturesData} />
           </Accordion>
-          <Accordion
-            label={`Données du projet "${projectFeaturesData.name}"`}
-            isOpen={isProjectFeaturesOpen}
-            onClick={toggleProjectFeatures}
-          >
+          <Accordion label={`Données du projet "${projectFeaturesData.name}"`} defaultOpen>
             <ProjectFeaturesView projectData={projectFeaturesData} />
           </Accordion>
         </div>
--- apps/web/src/features/public-pages/landings/sections/FaqSection.tsx
+++ apps/web/src/features/public-pages/landings/sections/FaqSection.tsx
@@ -1,52 +1,9 @@
 import Button from "@codegouvfr/react-dsfr/Button";
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 
-import classNames from "@/shared/views/clsx";
+import Accordion from "@/shared/views/components/Accordion/Accordion";
 
 import SectionTitle from "./SectionTitle";
-
-type AccordionProps = {
-  label: string;
-  children: ReactNode;
-};
-
-function Accordion({ label, children }: AccordionProps) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => {
-    setIsOpen((isOpen) => !isOpen);
-  };
-
-  return (
-    <div>
-      <div
-        className={classNames(
-          "py-2 px-4 mb-4",
-          "rounded-sm border border-solid border-transparent",
-          "bg-impacts-main dark:bg-blue-dark",
-          "cursor-pointer",
-          "transition ease-in-out duration-500",
-          "hover:border-grey-dark hover:dark:border-white",
-          "flex items-center gap-2",
-        )}
-        onClick={toggle}
-      >
-        <Button
-          className={classNames("text-black dark:text-white")}
-          iconId={isOpen ? "fr-icon-arrow-up-s-fill" : "fr-icon-arrow-down-s-fill"}
-          onClick={(e) => {
-            e.preventDefault();
-          }}
-          size="small"
-          priority="tertiary no outline"
-          title={isOpen ? "Fermer la section" : "Afficher la section"}
-        />
-        <h2 className="text-lg mb-0">{label}</h2>
-      </div>
-      {isOpen && <div className="px-4">{children}</div>}
-    </div>
-  );
-}
 
 type FaqItem = {
   question: string;
