50f5076e46dc4de4845568dcd5a4601464b82313
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-11-14 11:02:30 +0100

    feat(api): send right body to Connect CRM when creating contact with newsletter subscription

--- apps/api/src/marketing/adapters/secondary/ConnectCrm.ts
+++ apps/api/src/marketing/adapters/secondary/ConnectCrm.ts
@@ -1,6 +1,7 @@
 import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { format } from "date-fns";
 import { lastValueFrom } from "rxjs";
 
 import { CRMGateway, NewContactProps } from "src/marketing/core/CRMGateway";
@@ -12,13 +13,21 @@ export class ConnectCrm implements CRMGateway {
     private readonly config: ConfigService,
   ) {}
   async createContact(props: NewContactProps): Promise<void> {
-    const body = {
+    const baseBody = {
       email: props.email,
       prenom: props.firstName,
       nom: props.lastName,
-      abonnementNewsletter: props.subscribedToNewsletter,
       source: "Bénéfriches",
+      acceptationRGPD: true,
     };
+
+    const body = props.subscribedToNewsletter
+      ? {
+          ...baseBody,
+          abonnementNewsletter: true,
+          dateNewsletter: format(new Date(), "yyyy-MM-dd"),
+        }
+      : baseBody;
 
     await lastValueFrom(
       this.httpClient.post(`${this.config.get("CONNECT_CRM_HOST")}/api/v1/personnes`, body, {
