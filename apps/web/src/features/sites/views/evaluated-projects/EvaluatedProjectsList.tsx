999da0bb0e60505e357ccb19f4a8983f5b283d4a
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-11-27 18:24:35 +0100

    feat(web): display 'Évaluer un nouveau projet' button when no projects on site page

--- apps/web/src/features/sites/views/evaluated-projects/EvaluatedProjectsList.tsx
+++ apps/web/src/features/sites/views/evaluated-projects/EvaluatedProjectsList.tsx
@@ -1,5 +1,3 @@
-import classNames from "@/shared/views/clsx";
-
 import { SiteView } from "../../core/site.types";
 import NewProjectTile from "./NewProjectTile";
 import EvaluatedProjectTile from "./ProjectTile";
@@ -10,15 +8,6 @@ type Props = {
 };
 
 export default function EvaluatedProjectsList({ siteId, projects }: Props) {
-  if (projects.length === 0) {
-    return (
-      <div className={classNames("fr-container", "py-6")}>
-        <h3 className="text-2xl">Projets évalués</h3>
-        <p className="text-center">Aucun projet n'a été évalué pour ce site.</p>
-      </div>
-    );
-  }
-
   return (
     <section>
       <h3 className="text-2xl">Projets évalués</h3>
