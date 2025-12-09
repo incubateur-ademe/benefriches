29fb873b454a6032a291be31591ed2dc2eae3506
Claire Dhénin <claire.dhenin@nebulae.co>
2025-12-09 15:43:09 +0100

    fix(web): display error message if soils carbon storage cannot be computed in project forms

--- apps/web/src/shared/views/project-form/urban-project/spaces/soils-carbon-storage/index.tsx
+++ apps/web/src/shared/views/project-form/urban-project/spaces/soils-carbon-storage/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 
+import BackNextButtonsGroup from "@/shared/views/components/BackNextButtons/BackNextButtons";
 import LoadingSpinner from "@/shared/views/components/Spinner/LoadingSpinner";
 import { useAppDispatch, useAppSelector } from "@/shared/views/hooks/store.hooks";
+import WizardFormLayout from "@/shared/views/layout/WizardFormLayout/WizardFormLayout";
 import SoilsCarbonStorageComparison from "@/shared/views/project-form/common/soils-carbon-storage-comparison/SoilsCarbonStorageComparison";
 import { useProjectForm } from "@/shared/views/project-form/useProjectForm";
 
@@ -21,22 +23,46 @@ export default function UrbanProjectSoilsCarbonStorageContainer() {
     onFetchSoilsCarbonStorageDifference();
   }, [dispatch, onFetchSoilsCarbonStorageDifference]);
 
-  useEffect(() => {
-    if (loadingState === "error") {
-      onNext();
-    }
-  }, [loadingState, onNext]);
-
-  if (loadingState !== "success") {
-    return <LoadingSpinner />;
+  if (loadingState === "error") {
+    return (
+      <WizardFormLayout title="Stockage du carbone par les sols après aménagement">
+        <section className="bg-warning-ultralight dark:bg-warning-ultradark p-6 rounded-lg mb-4">
+          <span
+            aria-hidden="true"
+            className="fr-icon--lg fr-icon-error-warning-line before:dark:text-warning-ultralight before:text-warning-ultradark flex mb-4"
+          ></span>
+          <p className="text-xl font-black">Bénéfriches n'a pas pu effectuer le calcul...</p>
+          Des données nécessaires au calcul de stockage du carbone sont manquantes&nbsp;:
+          <ol>
+            <li>Veuillez remplir la répartition des espaces si ce n'est pas fait.</li>
+            <li>
+              Bénéfriches n'a pas les données nécessaires pour calculer le stockage du carbone sur
+              des terrains hors France Métropolitaine
+            </li>
+          </ol>
+        </section>
+        <BackNextButtonsGroup onBack={onBack} onNext={onNext} />
+      </WizardFormLayout>
+    );
   }
 
-  return (
-    <SoilsCarbonStorageComparison
-      onNext={onNext}
-      onBack={onBack}
-      currentCarbonStorage={current!}
-      projectedCarbonStorage={projected!}
-    />
-  );
+  if (loadingState === "loading") {
+    return (
+      <>
+        <h2>Stockage du carbone par les sols après aménagement</h2>
+        <LoadingSpinner />
+      </>
+    );
+  }
+
+  if (loadingState === "success") {
+    return (
+      <SoilsCarbonStorageComparison
+        onNext={onNext}
+        onBack={onBack}
+        currentCarbonStorage={current!}
+        projectedCarbonStorage={projected!}
+      />
+    );
+  }
 }
