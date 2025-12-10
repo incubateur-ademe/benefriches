e22d016586840aa73e02a9cdb288212ba9a6e017
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2025-12-10 11:37:45 +0100

    refactor(e2e-tests): use homepage page object in test

--- apps/e2e-tests/tests/homepage.spec.ts
+++ apps/e2e-tests/tests/homepage.spec.ts
@@ -1,8 +1,10 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from "../fixtures/onboarding.fixtures";
+import { HomePage } from "../pages/HomePage";
 
 test.describe("Homepage", () => {
   test("displays the correct title", async ({ page }) => {
-    await page.goto("/");
+    const homePage = new HomePage(page);
+    await homePage.goto();
 
     await expect(page).toHaveTitle(
       "Bénéfriches, l'outil qui calcule la valeur réelle de votre projet d'aménagement",
