3a858ae4ec0a3a4e7bde2ffd02274c871b342e47
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2026-03-12 16:38:51 +0100

    fix(web): allow metabase iframe to load on stats page

--- apps/web/src/features/public-pages/stats/StatsPage.tsx
+++ apps/web/src/features/public-pages/stats/StatsPage.tsx
@@ -8,11 +8,13 @@ function StatsPage() {
       <HtmlTitle>Statistiques</HtmlTitle>
       <h1>Statistiques</h1>
       <iframe
+        title="Tableau de bord Metabase des statistiques Bénéfriches"
         src="https://benefriches-metabase.osc-secnum-fr1.scalingo.io/public/dashboard/ca0ba163-c12c-41bf-b0ea-2ce7672ae50a"
         className="border-0"
         width="100%"
         height="600"
-        sandbox=""
+        // eslint-disable-next-line react/iframe-missing-sandbox -- Metabase requires both allow-scripts and allow-same-origin
+        sandbox="allow-scripts allow-same-origin"
       ></iframe>
     </section>
   );
