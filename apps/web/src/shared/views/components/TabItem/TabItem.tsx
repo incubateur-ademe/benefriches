716736f434de663316ed2f025923a4254d3282c2
Claire Dhénin <claire.dhenin@nebulae.co>
2026-03-17 14:20:19 +0100

    feat(web): improve TabItem layout for small screens

--- apps/web/src/shared/views/components/TabItem/TabItem.tsx
+++ apps/web/src/shared/views/components/TabItem/TabItem.tsx
@@ -2,32 +2,39 @@ import { FrIconClassName } from "@codegouvfr/react-dsfr";
 import { Link } from "type-route";
 
 import classNames from "../../clsx";
+import { useIsSmallScreen } from "../../hooks/useIsSmallScreen";
 
 type TabItemProps = {
   isActive: boolean;
-  label: React.ReactNode;
+  label: string;
   linkProps: Link;
   iconId: FrIconClassName;
 };
 
 function TabItem({ isActive, iconId, label, linkProps }: TabItemProps) {
+  const isSmallScreen = useIsSmallScreen();
   return (
     <li
       className={classNames(
         isActive
           ? "bg-blue-ultralight text-blue-ultradark dark:bg-blue-dark dark:text-blue-ultralight"
           : "bg-background-light text-[#4f4f4f] dark:bg-gray-700 dark:text-grey-light",
-        "px-4 py-2 text-sm font-medium rounded-t-lg",
+        "rounded-t-lg p-0",
       )}
     >
-      <a className="bg-none" {...linkProps}>
-        <span className="inline-flex items-center gap-2">
-          <i
-            className={`${iconId} fr-icon--md text-blue-dark dark:text-blue-ultralight`}
-            aria-hidden="true"
-          />
-          {label}
-        </span>
+      <a
+        title={isSmallScreen ? label : undefined}
+        className={classNames(
+          "rounded-t-lg inline-flex items-center gap-2",
+          "bg-none px-4 py-2 text-sm font-medium",
+        )}
+        {...linkProps}
+      >
+        <i
+          className={`${iconId} fr-icon--md text-blue-dark dark:text-blue-ultralight`}
+          aria-hidden="true"
+        />
+        {!isSmallScreen && label}
       </a>
     </li>
   );
