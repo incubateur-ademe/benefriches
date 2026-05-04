cedf148fc6493a456539d3253c72c3c1296ae5f3
Claire Dhénin <claire.dhenin@nebulae.co>
2026-05-04 15:17:31 +0200

    fix(web): wrong marker layout on break even chart in impacts page

--- apps/web/src/features/projects/views/project-break-even-level/charts/BreakEvenLevelChart.tsx
+++ apps/web/src/features/projects/views/project-break-even-level/charts/BreakEvenLevelChart.tsx
@@ -2,7 +2,7 @@ import Button from "@codegouvfr/react-dsfr/Button";
 import { Menu, MenuButton, MenuItems } from "@headlessui/react";
 import * as Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import { Fragment, useRef } from "react";
+import { Fragment, useMemo, useRef } from "react";
 
 import { withDefaultChartOptions } from "@/shared/views/charts";
 import useExportConfig from "@/shared/views/charts/useExportConfig";
@@ -25,6 +25,107 @@ type Props = {
 const BreakEvenLevelChart = ({ values, xValues, breakEvenIndex, breakEvenYear }: Props) => {
   const chartRef = useRef<HighchartsReact.RefObject>(null);
   const exportConfig = useExportConfig({ title: "📈 Évolution de la balance coût-bénéfice" });
+
+  const options: Highcharts.Options = useMemo(
+    () =>
+      withDefaultChartOptions({
+        chart: {
+          type: "area",
+          styledMode: true,
+          height: 500,
+        },
+        yAxis: {
+          lineWidth: 1,
+          startOnTick: false,
+          endOnTick: false,
+          tickPositions: [Math.min(...values), 0, Math.max(...values)],
+          tickWidth: 0,
+          plotLines: [
+            {
+              value: 0,
+              width: 1,
+              color: "#ccc",
+              zIndex: 2,
+            },
+          ],
+          title: { text: undefined },
+          labels: {
+            format: "{value:,.0f} €",
+          },
+          maxPadding: 0.1,
+        },
+        credits: {
+          enabled: false,
+        },
+        tooltip: {
+          useHTML: true,
+          formatter: function () {
+            return `<span class="fr-badge fr-badge--sm bg-blue-dark text-white">${this.key}</span> <span class='font-bold ${getPositiveNegativeTextClassesFromValue(this.y ?? 0)}'>${formatMonetaryImpact(this.y ?? 0)}</span>`;
+          },
+        },
+
+        plotOptions: {
+          series: {
+            events: {
+              legendItemClick: function () {
+                return false;
+              },
+            },
+          },
+          area: {
+            allowPointSelect: false,
+          },
+        },
+        xAxis: {
+          categories: xValues,
+          lineWidth: 0,
+          tickInterval: 5,
+          plotLines: [
+            { value: 0 },
+            {
+              value: breakEvenIndex,
+              dashStyle: "Dot",
+              zIndex: 5,
+              className: "breakeven-plotline",
+              label: {
+                rotation: 0,
+                useHTML: true,
+                text: `<div class="breakeven-badge">
+                        ${breakEvenYear}
+                        <span class="breakeven-badge__check">✓</span>
+                      </div>`,
+                verticalAlign: "bottom",
+                textAlign: "center",
+                y: -25,
+                x: 0,
+              },
+            },
+          ],
+        },
+        series: [
+          {
+            type: "area",
+            name: "Impacts socio-économiques moins le bilan de l’opération",
+            data: values.map((val, i) => ({
+              y: val,
+              marker:
+                i === breakEvenIndex
+                  ? {
+                      radius: 10,
+                      enabled: true,
+                    }
+                  : {
+                      lineColor: "transparent",
+                      fillColor: "transparent",
+                      radius: 0,
+                      enabled: false,
+                    },
+            })),
+          },
+        ],
+      }),
+    [values, xValues, breakEvenYear, breakEvenIndex],
+  );
 
   return (
     <div
@@ -74,108 +175,7 @@ const BreakEvenLevelChart = ({ values, xValues, breakEvenIndex, breakEvenYear }:
         highcharts={Highcharts}
         ref={chartRef}
         containerProps={{ className: "breakeven-graph" }}
-        options={withDefaultChartOptions({
-          chart: {
-            type: "area",
-            styledMode: true,
-            height: 500,
-          },
-          yAxis: {
-            lineWidth: 1,
-            startOnTick: false,
-            endOnTick: false,
-            tickPositions: [Math.min(...values), 0, Math.max(...values)],
-            tickWidth: 0,
-            plotLines: [
-              {
-                value: 0,
-                width: 1,
-                color: "#ccc",
-                zIndex: 2,
-              },
-            ],
-            title: { text: undefined },
-            labels: {
-              format: "{value:,.0f} €",
-            },
-            maxPadding: 0.1,
-          },
-          credits: {
-            enabled: false,
-          },
-          tooltip: {
-            useHTML: true,
-            formatter: function () {
-              return `<span class="fr-badge fr-badge--sm bg-blue-dark text-white">${this.key}</span> <span class='font-bold ${getPositiveNegativeTextClassesFromValue(this.y ?? 0)}'>${formatMonetaryImpact(this.y ?? 0)}</span>`;
-            },
-          },
-
-          plotOptions: {
-            series: {
-              events: {
-                legendItemClick: function () {
-                  return false;
-                },
-              },
-            },
-            area: {
-              allowPointSelect: false,
-
-              marker: {
-                enabled: false,
-                symbol: "circle",
-                radius: 2,
-                states: {
-                  hover: {
-                    enabled: true,
-                  },
-                },
-              },
-            },
-          },
-          xAxis: {
-            categories: xValues,
-            lineWidth: 0,
-            tickInterval: 5,
-            plotLines: [
-              { value: 0 },
-              {
-                value: breakEvenIndex,
-                dashStyle: "Dot",
-                zIndex: 5,
-                className: "breakeven-plotline",
-                label: {
-                  rotation: 0,
-                  useHTML: true,
-                  text: `<div class="breakeven-badge">
-                        ${breakEvenYear}
-                        <span class="breakeven-badge__check">✓</span>
-                      </div>`,
-                  verticalAlign: "bottom",
-                  textAlign: "center",
-                  y: -25,
-                  x: 0,
-                },
-              },
-            ],
-          },
-          series: [
-            {
-              type: "area",
-              name: "Impacts socio-économiques moins le bilan de l’opération",
-              data: values.map((val, i) => ({
-                y: val,
-                marker:
-                  xValues[i] === breakEvenYear
-                    ? {
-                        enabled: true,
-                        className: "breakeven-marker",
-                      }
-                    : { enabled: false },
-              })),
-            },
-          ],
-        })}
+        options={options}
       />
     </div>
   );
