be075fbd7fa1cd4d6b455c48f3d3c792622159b4
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2026-05-12 12:15:43 +0200

    feat(web): show total site surface area on public green spaces step

--- apps/web/src/shared/views/project-form/urban-project/uses/public-green-spaces-surface-area/PublicGreenSpacesSurfaceArea.tsx
+++ apps/web/src/shared/views/project-form/urban-project/uses/public-green-spaces-surface-area/PublicGreenSpacesSurfaceArea.tsx
@@ -7,6 +7,7 @@ import {
 import BackNextButtonsGroup from "@/shared/views/components/BackNextButtons/BackNextButtons";
 import RowDecimalsNumericInput from "@/shared/views/components/form/NumericInput/RowDecimalsNumericInput";
 import { requiredNumericFieldRegisterOptions } from "@/shared/views/components/form/NumericInput/registerOptions";
+import FormInfo from "@/shared/views/layout/WizardFormLayout/FormInfo";
 import WizardFormLayout from "@/shared/views/layout/WizardFormLayout/WizardFormLayout";
 
 type Props = {
@@ -27,7 +28,14 @@ function PublicGreenSpacesSurfaceArea({ initialValue, siteSurfaceArea, onSubmit,
   });
 
   return (
-    <WizardFormLayout title="Quelle superficie feront les espaces verts publics&nbsp;?">
+    <WizardFormLayout
+      title="Quelle superficie feront les espaces verts publics&nbsp;?"
+      instructions={
+        <FormInfo>
+          Pour rappel, la superficie totale du site est de {formatSurfaceArea(siteSurfaceArea)}.
+        </FormInfo>
+      }
+    >
       <form
         onSubmit={handleSubmit(({ surfaceArea }) => {
           onSubmit(surfaceArea);
